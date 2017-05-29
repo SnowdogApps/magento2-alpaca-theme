f14abefbe752aa02e7c44819e8495feddf7ee244
Ania Karon <anna.karon@snow.dog>
2017-05-29 18:01:30 +0200

    general theme settings - styles from components, libs and polyfills

---
+++ Magento_Theme/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    map: {
+        "*": {
+            "closestPolyfill" : "Magento_Theme/js/lib/closest-polyfill",
+            "lazysizes"       : "Magento_Theme/js/lib/lazysizes.min",
+            "picturefill"     : "Magento_Theme/js/lib/picturefill",
+            "slick"           : "Magento_Theme/js/lib/slick",
+            "svg4everybody"   : "Magento_Theme/js/lib/svg4everybody"
+        }
+    }
+};
