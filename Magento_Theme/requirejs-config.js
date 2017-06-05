703ba444300ee98b256712a85deb50cb4805dcbf
Ania Karon <anna.karon@snow.dog>
2017-06-05 16:30:51 +0200

    header top links

--- Magento_Theme/requirejs-config.js
+++ Magento_Theme/requirejs-config.js
@@ -2,10 +2,11 @@ var config = {
     map: {
         "*": {
             "closestPolyfill" : "Magento_Theme/js/lib/closest-polyfill",
-            "lazysizes"       : "Magento_Theme/js/lib/lazysizes.min",
+            "lazysizes"       : "Magento_Theme/js/lib/lazysizes",
             "picturefill"     : "Magento_Theme/js/lib/picturefill",
             "slick"           : "Magento_Theme/js/lib/slick",
-            "svg4everybody"   : "Magento_Theme/js/lib/svg4everybody"
+            "svg4everybody"   : "Magento_Theme/js/lib/svg4everybody",
+            "linksWrapper"     : "Magento_Theme/js/wrapper-collapsible"
         }
     }
 };
