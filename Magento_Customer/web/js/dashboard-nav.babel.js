9c5f24767c8baf3241019ce7bdcfc64bf8850174
Filip Kudła <filip.kudla@snow.dog>
2018-02-05 14:57:41 +0100

    #37752 - Update script from dashboard-nav left side drpodown on mobile

--- Magento_Customer/web/js/dashboard-nav.babel.js
+++ Magento_Customer/web/js/dashboard-nav.babel.js
@@ -1,15 +1,25 @@
-'use strict'
+define(() => (config, element) => {
+  'use strict'
 
-const content     = document.querySelector('.dashboard-nav__content'),
-      currentItem = content.querySelector('.dashboard-nav__list .item.current'),
-      mobileNav   = document.querySelector('.dashboard-nav__mobile'),
-      windowSize  = window.matchMedia('(max-width: 768px)');
+  const content     = element.querySelector('.dashboard-nav__content'),
+        currentItem = content.querySelector('.dashboard-nav__list .item.current'),
+        mobileNav   = element.querySelector('.dashboard-nav__mobile'),
+        windowSize  = window.matchMedia('(max-width: 768px)');
 
-if (windowSize.match) {
-  mobileNav.innerHTML = currentItem.textContent;
+  makeDropdown();
 
-  mobileNav.addEventListener('click', () => {
-    mobileNav.classList.toggle('dashboard-nav__mobile--active');
-    content.classList.toggle('dashboard-nav__content--visible');
+  window.addEventListener('resize', () => {
+    makeDropdown();
   });
-}
+
+  function makeDropdown() {
+    if (windowSize.matches) {
+      mobileNav.innerHTML = currentItem.textContent;
+
+      mobileNav.addEventListener('click', () => {
+        mobileNav.classList.toggle('dashboard-nav__mobile--active');
+        content.classList.toggle('dashboard-nav__content--visible');
+      });
+    }
+  }
+});
