53c81ade94819f067b79a33e3efac18cbec30e06
Bartek Igielski <igloczek@gmail.com>
2018-02-09 04:07:54 +0100

    Selects opening on bottom. Default sorting function with support for local characters

--- Magento_Theme/web/js/select.babel.js
+++ Magento_Theme/web/js/select.babel.js
@@ -3,9 +3,13 @@ define(['choices'], function(Choices) {
 
     return function(options, select) {
         const defaultOptions = {
-            placeholder  : false,
+            placeholder: false,
             searchEnabled: false,
-            classNames   : {
+            position: 'bottom',
+            sortFilter: (a, b) => {
+              return a.label.localeCompare(b.label);
+            },
+            classNames: {
                 containerInner: 'select__field',
                 list          : 'select__field-list',
                 input         : 'select__field-input',
