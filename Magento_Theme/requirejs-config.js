54926d6f10a1c1d4455c599fb63de5d6aa3d0538
Filip Kudła <filip.kudla@snow.dog>
2018-03-07 18:35:02 +0100

    #39434 - Add lory lib and add slider script with dep to it + delete slick lib

--- Magento_Theme/requirejs-config.js
+++ Magento_Theme/requirejs-config.js
@@ -1,17 +1,27 @@
 var config = {
-    map: {
-        "*": {
-            "closestPolyfill" : "Magento_Theme/js/lib/closest-polyfill",
-            "dropdownList"    : "Magento_Theme/js/dropdown-list.babel",
-            "fileUpload"      : "Magento_Theme/js/file-upload.babel",
-            "lazysizes"       : "Magento_Theme/js/lib/lazysizes",
-            "picturefill"     : "Magento_Theme/js/lib/picturefill",
-            "slick"           : "Magento_Theme/js/lib/slick",
-            "svg4everybody"   : "Magento_Theme/js/lib/svg4everybody",
-            "choices"         : "Magento_Theme/js/lib/choices",
-            "linksWrapper"    : "Magento_Theme/js/wrapper-collapsible",
-            "select"          : "Magento_Theme/js/select.babel",
-            "tab"             : "Magento_Theme/js/tab.babel"
-        }
+  map: {
+    "*": {
+      "closestPolyfill" : "Magento_Theme/js/lib/closest-polyfill",
+      "dropdownList"    : "Magento_Theme/js/dropdown-list.babel",
+      "fileUpload"      : "Magento_Theme/js/file-upload.babel",
+      "lazysizes"       : "Magento_Theme/js/lib/lazysizes",
+      "picturefill"     : "Magento_Theme/js/lib/picturefill",
+      "svg4everybody"   : "Magento_Theme/js/lib/svg4everybody",
+      "choices"         : "Magento_Theme/js/lib/choices",
+      "linksWrapper"    : "Magento_Theme/js/wrapper-collapsible",
+      "lory"            : "Magento_Theme/js/lib/lory",
+      "select"          : "Magento_Theme/js/select.babel",
+      "slider"          : "Magento_Theme/js/slider.babel",
+      "tab"             : "Magento_Theme/js/tab.babel"
     }
+  },
+  shim: {
+    "slider": {
+       "deps": ["lory"]
+    }
+  },
+  deps: [
+    "lory",
+    "slider"
+  ]
 };
