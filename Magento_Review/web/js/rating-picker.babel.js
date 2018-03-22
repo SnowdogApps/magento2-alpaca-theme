520d5c593dce8264a28586724c6f25a40e54d111
Filip Kudła <filip.kudla@snow.dog>
2018-03-22 20:12:38 +0100

    #39434 - Update adding review form and script to showing property errors

--- Magento_Review/web/js/rating-picker.babel.js
+++ Magento_Review/web/js/rating-picker.babel.js
@@ -2,18 +2,29 @@ define([], function() {
   return function(config, element) {
     'use strict'
 
-    const ratingElements = [...element.querySelectorAll('.rating__rate-item')];
+    const activeItemClass   = 'rating__rate-item--active',
+          errorVisibleClass = 'rating__error--visible',
+          reviewError       = document.getElementById(config.reviewError),
+          ratingElements    = [...element.querySelectorAll('.rating__rate-item')],
+          submitButton      = document.getElementById(config.submitButton);
+
+    let optionChoosen = false;
 
     ratingElements.forEach(oneRank => {
       oneRank.addEventListener('click', () => {
         deleteOtherRates();
 
         oneRank.querySelector('.radio__field').checked = true;
-        oneRank.classList.add('rating__rate-item--active');
+        oneRank.classList.add(activeItemClass);
+        optionChoosen = true;
+
+        if (reviewError.classList.contains(errorVisibleClass)) {
+          reviewError.classList.remove(errorVisibleClass);
+        }
 
         for (let i = 0; i < ratingElements.length; i++) {
-          if (!ratingElements[i].classList.contains('rating__rate-item--active')) {
-            ratingElements[i].classList.add('rating__rate-item--active');
+          if (!ratingElements[i].classList.contains(activeItemClass)) {
+            ratingElements[i].classList.add(activeItemClass);
           }
           else {
             return;
@@ -22,9 +33,15 @@ define([], function() {
       });
     });
 
+    submitButton.addEventListener('click', () => {
+      if (!optionChoosen) {
+        reviewError.classList.add(errorVisibleClass);
+      }
+    });
+
     function deleteOtherRates() {
       ratingElements.forEach(item => {
-        item.classList.remove('rating__rate-item--active');
+        item.classList.remove(activeItemClass);
       });
     }
   }
