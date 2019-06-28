8d16d0f154022a3e1437882a2d809395beef6294
Bartek Igielski <igloczek@gmail.com>
2019-06-28 19:50:02 +0200

    E2E tests code cleanup. ESLint setup. CI linting setup.

--- .tests/cypress/support/commands.js
+++ .tests/cypress/support/commands.js
@@ -24,76 +24,69 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-
 Cypress.Commands.add('login', () => {
-    cy.account()
-    cy.get('#email').type('qwe@qwe.com').should('have.value', 'qwe@qwe.com')
-    cy.get('#pass').type('Qweqwe_1').should('have.value', 'Qweqwe_1')
-    cy.get('[data-testid=submit-login-button]').click()
+  cy.account()
+  cy.get('#email').type('qwe@qwe.com').should('have.value', 'qwe@qwe.com')
+  cy.get('#pass').type('Qweqwe_1').should('have.value', 'Qweqwe_1')
+  cy.get('[data-testid=submit-login-button]').click()
 })
 
 Cypress.Commands.add('account', () => {
-    cy.visit('https://alpaca-ce-solr-demo.snowdog.pro/')
-    cy.get('[data-testid=customer-account-link]').click()
-    cy.url().should('include', '/customer/account/')
-    cy.get('.login__form').should('be.visible')
+  cy.visit('https://alpaca-ce-solr-demo.snowdog.pro/')
+  cy.get('[data-testid=customer-account-link]').click()
+  cy.url().should('include', '/customer/account/')
+  cy.get('.login__form').should('be.visible')
 })
 
-
 Cypress.Commands.add('randomcategory', () => {
-    cy.get('.mega-menu__link')
-        .then((elements) => {
-            cy.log(elements)
-            elements[Math.floor(Math.random() * elements.length)]
-                .click(elements)
-        })
-
+  cy.get('.mega-menu__link')
+    .then((elements) => {
+      cy.log(elements)
+      elements[Math.floor(Math.random() * elements.length)]
+        .click(elements)
+    })
 })
 
 Cypress.Commands.add('gotoproductpage', () => {
-    cy.get('[data-testid=catalog-grid-item__link]')
-        .then((elements) => {
-            cy.log(elements)
-            elements[Math.floor(Math.random() * elements.length)]
-                .click(elements)
-        })
+  cy.get('[data-testid=catalog-grid-item__link]')
+    .then((elements) => {
+      cy.log(elements)
+      elements[Math.floor(Math.random() * elements.length)]
+        .click(elements)
+    })
 })
 
 Cypress.Commands.add('addproducttocart', () => {
-    cy.get('.breadcrumbs__item')
-    
-    cy.get('.size').find('.swatch__option').as('Size')
-        .then((elements) => {
-            cy.log(elements)
-            elements[Math.floor(Math.random() * elements.length)]
-                .click(elements)
-        })
-      
-    cy.get('.color').find('.swatch__option')
-        .then((elements) => {
-            cy.log(elements)
-            elements[Math.floor(Math.random() * elements.length)]
-                .click(elements)
-        })
-       
-    cy.get('#product-addtocart-button').first()
-        .click()   
+  cy.get('.breadcrumbs__item')
+
+  cy.get('.size').find('.swatch__option').as('Size')
+    .then((elements) => {
+      cy.log(elements)
+      elements[Math.floor(Math.random() * elements.length)]
+        .click(elements)
     })
 
+  cy.get('.color').find('.swatch__option')
+    .then((elements) => {
+      cy.log(elements)
+      elements[Math.floor(Math.random() * elements.length)]
+        .click(elements)
+    })
 
-Cypress.Commands.add('MassageSuccess',()=> {
-    cy.get('[data-ui-id=message-success]').should('be.visible')
+  cy.get('#product-addtocart-button').first()
+    .click()
+})
+
+Cypress.Commands.add('MassageSuccess', () => {
+  cy.get('[data-ui-id=message-success]').should('be.visible')
     .log('product added to cart')
 })
 
-
-
 Cypress.Commands.add('gotocheckout', () => {
-    cy.get('[data-testid=minicart-link]').click()
-    cy.get('[data-testid=view-cart-link]').should('be.visible').click()
-    cy.get('#coupon_code')
-    cy.get('#cart-totals').should('have.class','.table-wrapper')
-    cy.get('.base')
-    cy.get('[data-testid=proceed-to-checkout-button]').click('center', { force: true })
+  cy.get('[data-testid=minicart-link]').click()
+  cy.get('[data-testid=view-cart-link]').should('be.visible').click()
+  cy.get('#coupon_code')
+  cy.get('#cart-totals').should('have.class', '.table-wrapper')
+  cy.get('.base')
+  cy.get('[data-testid=proceed-to-checkout-button]').click('center', { force: true })
 })
-
