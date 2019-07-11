392035398c94fce83e8c0f73d94fb88892d95958
Ebonom Mfam <ebonom.mfam@snow.dog>
2019-07-11 13:48:46 +0200

    54485 - added login tests

---
+++ .tests/cypress/integration/log_in/login/login_validation.js
@@ -0,0 +1,35 @@
+var faker = require('faker')
+
+describe('Login validation test', () => {
+    before(() => {
+      cy.account()
+    })
+
+    it('checks login form is visble', () => {
+        cy.get('.login__form').should('be.visible')
+        cy.get('[data-testid="forgot-password-link"').should('be.visible')
+        cy.get('[data-testid="submit-login-button"]').should('be.visible')
+    })
+
+    it('submits of empty form', () => {
+        cy.submit_login_form()
+        cy.get('.email-error').should('be.visible') 
+        cy.get('.pass-error').should('be.visible')
+    })
+
+    it('fills ONLY email address and submits form', () => {
+        cy.get('#email').type(faker.internet.email())
+        cy.submit_login_form()
+        cy.get('#pass-error').should('be.visible')
+        .and('have.text', 'This is a required field.')
+    })
+
+    it('fills ONLY password and submits form', () => {
+        cy.get('#email').clear()
+        cy.get('#pass').type(faker.internet.password())
+        cy.submit_login_form()
+        cy.get('#email-error').should('be.visible') 
+        .and('have.text', 'This is a required field.')
+    })
+    
+})
---
+++ .tests/cypress/integration/log_in/registration/registration.js
@@ -0,0 +1,34 @@
+var faker = require('faker')
+
+describe('Registration test', () => {
+    before(() => {
+      cy.account()
+    })
+
+    it('checks New Customer header title is visible', () => {
+        cy.contains('New Customers').should('be.visible')
+    })
+
+    it('checks that Create an Account button is visble', () => {
+        cy.get('[data-testid="create-customer-account-link"]').should('be.visible')
+    })
+
+    it('redirects to create account page', () => {
+        cy.get('[data-testid="create-customer-account-link"]').click()
+        cy.url().should('include', '/customer/account/create/')
+    })
+
+    it('submits registration form', () => {
+        cy.get('form[id="form-validate"]').should('be.visible')
+        cy.get('#firstname').type(faker.name.firstName())
+        cy.get('#lastname').type(faker.name.lastName())
+        cy.get('div[class="checkbox registration__newsletter-checkbox"]').click()
+        cy.get('#email_address').type(faker.internet.email())
+        cy.get('#password').type('Password123')
+        cy.get('#password-confirmation').type('Password123')
+        cy.get('[data-testid="create-account-button"]').click()
+        cy.url().should('include', '/customer/account/index/')
+        cy.get('div[class="row"]').should('be.visible')
+    })
+
+})
