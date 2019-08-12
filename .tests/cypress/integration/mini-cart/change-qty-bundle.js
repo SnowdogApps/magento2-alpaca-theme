cbae8cdf1fbf7d2d78488b94a3c7724d1f9e5383
Anna Krzyżańska <anna.krzyzanska@gmail.com>
2019-08-12 15:06:06 +0200

    #54485 Change qty - minicart test

---
+++ .tests/cypress/integration/mini-cart/change-qty-bundle.js
@@ -0,0 +1,85 @@
+describe('Bundle product', function () {
+
+    before(() => {
+
+        // Hide premissions popup
+        cy.setCookie('permission-cookies', 'true')
+        cy.setCookie('permission-profiling', 'true')
+        cy.setCookie('mage-cache-sessid', 'true')
+        // Keep cookies beween tests
+        Cypress.Cookies.defaults({
+            whitelist: [
+                'frontend',
+                'X-Magento-Vary',
+                'permission-profiling',
+                'PHPSESSID',
+                'form_key',
+            ]
+        })
+    })
+
+    it('Visits product', () => {
+        cy.visit('https://alpaca-ce-solr-demo.snowdog.pro/sprite-yoga-companion-kit')
+        cy.get('.breadcrumbs__list').should('be.visible')
+    })
+
+    it('Check visiblility of content', () => {
+        cy.get('.product-view__main-details').should('be.visible')
+        cy.get('[data-ui-id=page-title-wrapper]').should('be.visible')
+        cy.get('.product-view__information').should('be.visible')
+        cy.get('.product-view__sku').should('be.visible')
+        cy.get('.price-box').should('be.visible')
+        cy.get('#product-options-wrapper').should('be.visible')
+        cy.get('.bundle-option').should('be.visible')
+        cy.get('.quantity-update').should('be.visible')
+        cy.get('#product-addtocart-button').should('be.visible')
+        cy.get('[data-testid=product-tab__title]').contains('Details').should('be.visible')
+        cy.get('[data-testid=product-tab__title]').contains('Reviews').should('be.visible')
+
+    })
+
+    it('Test product options', () => {
+        cy.get('#product-options-wrapper').should('be.visible')
+            .then((elements) => {
+                cy.log(elements.length)
+            })
+        cy.get('.radio__fieldset').find('.radio__label')
+        .click({
+                multiple: true
+            })
+            .then((elements) => {
+                cy.log(elements.length)
+            })
+        cy.get('#product-addtocart-button').first().click()
+        cy.server()
+        cy.route('/customer/section/load/?sections=cart*').as('addToCart')
+        cy.wait('@addToCart')
+    })
+
+    it('Check if mini-cart is not empty', () => {
+        cy.get('[data-testid=minicart-link]').click()
+        cy.get('#minicart-content-wrapper')
+        cy.contains('You have no items in your shopping cart.').should('not.be.visible')
+    })
+
+    it('Change product qty in cart', () => {
+        cy.get('.cart-item-qty').clear().type('2')
+        cy.get('.minicart-product__info').contains('Update').click()
+    })
+
+    it('Qty check', () => {    
+        cy.get('[data-testid=minicart-link]').click()
+        cy.get('.cart-item-qty').should('eq','2')
+    })
+
+    after(() => {
+        // Clear cookie after tests to enable running test several times
+        cy.clearCookie('frontend')
+        cy.clearCookie('permission-cookies')
+        cy.clearCookie('permission-profiling')
+        cy.clearCookie('form_key')
+        cy.clearCookie('PHPSESSID')
+        cy.clearCookie('mage-cache-sessid')
+    })
+
+})
---
+++ .tests/cypress/integration/mini-cart/change-qty-grouped.js
@@ -0,0 +1,97 @@
+describe('Grouped product', function () {
+
+    before(() => {
+
+        // Hide premissions popup
+        cy.setCookie('permission-cookies', 'true')
+        cy.setCookie('permission-profiling', 'true')
+        cy.setCookie('mage-cache-sessid', 'true')
+        // Keep cookies beween tests
+        Cypress.Cookies.defaults({
+            whitelist: [
+                'frontend',
+                'X-Magento-Vary',
+                'permission-profiling',
+                'PHPSESSID',
+                'form_key',
+            ]
+        })
+    })
+
+    it('Visits product', () => {
+        cy.visit('https://alpaca-ce-solr-demo.snowdog.pro/grouped-product-test')
+        cy.get('.breadcrumbs__list').should('be.visible')
+    })
+
+    it('Check visiblility of content', () => {
+        cy.get('.product-view__main-details').should('be.visible')
+        cy.get('[data-ui-id=page-title-wrapper]').should('be.visible')
+        cy.get('.product-view__information').should('be.visible')
+        cy.get('.product-view__sku').should('be.visible').log('SKU VISIBLE')
+        cy.get('#super-product-table').should('be.visible').log('Grouped product table visible')
+        cy.get('#product-addtocart-button').should('be.visible')
+        cy.get('.product-view__extra-actions').should('be.visible')
+        cy.get('[data-testid=product-tab__title]').contains('Details').should('be.visible')
+        cy.get('[data-testid=product-tab__title]').contains('Reviews').should('be.visible')
+        cy.get('.fotorama__img')
+
+    })
+
+    it('Test grouped product table', () => {
+        cy.get('#super-product-table').should('be.visible').find('>tbody')
+            .then((elements) => {
+                cy.log(elements.length)
+            })
+        cy.get('.product-view__grouped-product-name')
+            .then((productsname) => {
+                cy.log(productsname.length)
+            })
+        cy.get('[data-price-type=finalPrice]')
+            .then((productsname) => {
+                cy.log(productsname.length)
+            })
+        cy.get('.qty')
+            .then((productsname) => {
+                cy.log(productsname.length)
+            })
+    })
+
+    it('Choose product from table', () => {
+        cy.get('#super-product-table')
+        cy.get('.input__field.input-text.qty')
+            .click({ multiple: true })
+            .clear()
+           
+          cy.get('.input__field.input-text.qty').eq(0).type('1')
+          cy.get('.input__field.input-text.qty').eq(1).type('1')
+          cy.get('.input__field.input-text.qty').eq(2).type('1')
+    })
+
+    it('Add product to cart', () => {
+        cy.server({ whitelist: () => false })
+        cy.get('#product-addtocart-button').click()
+    })
+
+    it('Change product qty in cart', () => {
+        cy.get('.cart-item-qty').clear().type('2')
+        cy.get('.minicart-product__info').contains('Update').click()
+    })
+
+    it('Qty check', () => {    
+        cy.get('[data-testid=minicart-link]').click()
+        cy.get('.cart-item-qty').should('eq','2')
+    })
+
+
+
+    after(() => {
+        // Clear cookie after tests to enable running test several times
+        cy.clearCookie('frontend')
+        cy.clearCookie('permission-cookies')
+        cy.clearCookie('permission-profiling')
+        cy.clearCookie('form_key')
+        cy.clearCookie('PHPSESSID')
+        cy.clearCookie('mage-cache-sessid')
+    })
+
+})
