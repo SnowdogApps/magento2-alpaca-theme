e3f0ee853f0cf881f0b5ed052f6323c4266d0f5e
Anna Krzyżańska <anna.krzyzanska@gmail.com>
2019-08-12 15:48:06 +0200

    #54485 Add to cart - mini-cart test - mini-cart test
    Code formatting

--- .tests/cypress/integration/mini-cart/add-to-cart-simple.js
+++ .tests/cypress/integration/mini-cart/add-to-cart-simple.js
@@ -1,60 +1,58 @@
 describe('Simple product', function () {
 
-    before(() => {
+  before(() => {
 
-        // Hide premissions popup
-        cy.setCookie('permission-cookies', 'true')
-        cy.setCookie('permission-profiling', 'true')
-        cy.setCookie('mage-cache-sessid', 'true')
-        // Keep cookies beween tests
-        Cypress.Cookies.defaults({
-            whitelist: [
-                'frontend',
-                'X-Magento-Vary',
-                'permission-profiling',
-                'PHPSESSID',
-                'form_key',
-            ]
-        })
+    // Hide premissions popup
+    cy.setCookie('permission-cookies', 'true')
+    cy.setCookie('permission-profiling', 'true')
+    cy.setCookie('mage-cache-sessid', 'true')
+    // Keep cookies beween tests
+    Cypress.Cookies.defaults({
+      whitelist: [
+        'frontend',
+        'X-Magento-Vary',
+        'permission-profiling',
+        'PHPSESSID',
+        'form_key',
+      ]
     })
+  })
 
-    it('Visits product', () => {
-        cy.visit('https://alpaca-ce-solr-demo.snowdog.pro/joust-duffle-bag')
-        cy.get('.breadcrumbs__list').should('be.visible')
-    })
+  it('Visits product', () => {
+    cy.visit('https://alpaca-ce-solr-demo.snowdog.pro/joust-duffle-bag')
+    cy.get('.breadcrumbs__list').should('be.visible')
+  })
 
-    it('Check visiblility of content', () => {
-        cy.get('.product-view__main-details').should('be.visible')
-        cy.get('[data-ui-id=page-title-wrapper]').should('be.visible')
-        cy.get('.product-view__information').should('be.visible')
-        cy.get('.product-view__sku').should('be.visible')
-        cy.get('#product-price-1').should('be.visible')
-        cy.get('.quantity-update').should('be.visible')
-        cy.get('#product-addtocart-button').should('be.visible')
-        cy.get('.product-view__extra-actions').should('be.visible')
-        cy.get('[data-testid=product-tab__title]').contains('Details').should('be.visible')
-        cy.get('[data-testid=product-tab__title]').contains('Reviews').should('be.visible')
+  it('Check visiblility of content', () => {
+    cy.get('.product-view__main-details').should('be.visible')
+    cy.get('[data-ui-id=page-title-wrapper]').should('be.visible')
+    cy.get('.product-view__information').should('be.visible')
+    cy.get('.product-view__sku').should('be.visible')
+    cy.get('#product-price-1').should('be.visible')
+    cy.get('.quantity-update').should('be.visible')
+    cy.get('#product-addtocart-button').should('be.visible')
+    cy.get('.product-view__extra-actions').should('be.visible')
+    cy.get('[data-testid=product-tab__title]').contains('Details').should('be.visible')
+    cy.get('[data-testid=product-tab__title]').contains('Reviews').should('be.visible')
+  })
 
-    })
+  it('Add product to cart', () => {
+    cy.get('#product-addtocart-button').first().click()
+  })
 
-    it('Add product to cart', () => {
-        cy.get('#product-addtocart-button').first().click()
-    })
+  it('Check if mini-cart is not empty', () => {
+    cy.get('[data-testid=minicart-link]').click()
+    cy.get('#minicart-content-wrapper')
+    cy.contains('You have no items in your shopping cart.').should('not.be.visible')
+  })
 
-    it('Check if mini-cart is not empty', () => {
-        cy.get('[data-testid=minicart-link]').click()
-        cy.get('#minicart-content-wrapper')
-        cy.contains('You have no items in your shopping cart.').should('not.be.visible')
-    })
-
-
-    after(() => {
-        // Clear cookie after tests to enable running test several times
-        cy.clearCookie('frontend')
-        cy.clearCookie('permission-cookies')
-        cy.clearCookie('permission-profiling')
-        cy.clearCookie('form_key')
-        cy.clearCookie('PHPSESSID')
-        cy.clearCookie('mage-cache-sessid')
-    })
+  after(() => {
+    // Clear cookie after tests to enable running test several times
+    cy.clearCookie('frontend')
+    cy.clearCookie('permission-cookies')
+    cy.clearCookie('permission-profiling')
+    cy.clearCookie('form_key')
+    cy.clearCookie('PHPSESSID')
+    cy.clearCookie('mage-cache-sessid')
+  })
 })
--- .tests/cypress/integration/mini-cart/add-to-cart-virtual.js
+++ .tests/cypress/integration/mini-cart/add-to-cart-virtual.js
@@ -1,60 +1,60 @@
 describe('Virtual product', function () {
 
-    before(() => {
+  before(() => {
 
-        // Hide premissions popup
-        cy.setCookie('permission-cookies', 'true')
-        cy.setCookie('permission-profiling', 'true')
-        cy.setCookie('mage-cache-sessid', 'true')
-        // Keep cookies beween tests
-        Cypress.Cookies.defaults({
-            whitelist: [
-                'frontend',
-                'X-Magento-Vary',
-                'permission-profiling',
-                'PHPSESSID',
-                'form_key',
-            ]
-        })
+    // Hide premissions popup
+    cy.setCookie('permission-cookies', 'true')
+    cy.setCookie('permission-profiling', 'true')
+    cy.setCookie('mage-cache-sessid', 'true')
+    // Keep cookies beween tests
+    Cypress.Cookies.defaults({
+      whitelist: [
+        'frontend',
+        'X-Magento-Vary',
+        'permission-profiling',
+        'PHPSESSID',
+        'form_key',
+      ]
     })
+  })
 
-    it('Visits product', () => {
-        cy.visit('https://alpaca-ce-solr-demo.snowdog.pro/virtual-product-test')
-        cy.get('.breadcrumbs__list').should('be.visible')
-    })
+  it('Visits product', () => {
+    cy.visit('https://alpaca-ce-solr-demo.snowdog.pro/virtual-product-test')
+    cy.get('.breadcrumbs__list').should('be.visible')
+  })
 
-    it('Check visiblility of content', () => {
-        cy.get('.product-view__main-details').should('be.visible')
-        cy.get('[data-ui-id=page-title-wrapper]').should('be.visible')
-        cy.get('.product-view__information').should('be.visible')
-        cy.get('.product-view__sku').should('be.visible')
-        cy.get('#product-price-2047').should('be.visible')
-        cy.get('.quantity-update').should('be.visible')
-        cy.get('#product-addtocart-button').should('be.visible')
-        cy.get('.product-view__extra-actions').should('be.visible')
-        cy.get('[data-testid=product-tab__title]').contains('Reviews').should('be.visible')
+  it('Check visiblility of content', () => {
+    cy.get('.product-view__main-details').should('be.visible')
+    cy.get('[data-ui-id=page-title-wrapper]').should('be.visible')
+    cy.get('.product-view__information').should('be.visible')
+    cy.get('.product-view__sku').should('be.visible')
+    cy.get('#product-price-2047').should('be.visible')
+    cy.get('.quantity-update').should('be.visible')
+    cy.get('#product-addtocart-button').should('be.visible')
+    cy.get('.product-view__extra-actions').should('be.visible')
+    cy.get('[data-testid=product-tab__title]').contains('Reviews').should('be.visible')
+  })
 
-    })
-    it('Add product to cart', () => {
-        cy.get('#product-addtocart-button').first().click()
-        cy.server()
-        cy.route('/customer/section/load/?sections=cart*').as('addToCart')
-        cy.wait(4000)
-        cy.wait('@addToCart')
-    })
+  it('Add product to cart', () => {
+    cy.get('#product-addtocart-button').first().click()
+    cy.server()
+    cy.route('/customer/section/load/?sections=cart*').as('addToCart')
+    cy.wait(4000)
+    cy.wait('@addToCart')
+  })
 
-    it('Check if mini-cart is not empty', () => {
-        cy.get('[data-testid=minicart-link]').click()
-        cy.get('#minicart-content-wrapper')
-        cy.contains('You have no items in your shopping cart.').should('not.be.visible')
-    })
-    after(() => {
-        // Clear cookie after tests to enable running test several times
-        cy.clearCookie('frontend')
-        cy.clearCookie('permission-cookies')
-        cy.clearCookie('permission-profiling')
-        cy.clearCookie('form_key')
-        cy.clearCookie('PHPSESSID')
-        cy.clearCookie('mage-cache-sessid')
-    })
+  it('Check if mini-cart is not empty', () => {
+    cy.get('[data-testid=minicart-link]').click()
+    cy.get('#minicart-content-wrapper')
+    cy.contains('You have no items in your shopping cart.').should('not.be.visible')
+  })
+  after(() => {
+    // Clear cookie after tests to enable running test several times
+    cy.clearCookie('frontend')
+    cy.clearCookie('permission-cookies')
+    cy.clearCookie('permission-profiling')
+    cy.clearCookie('form_key')
+    cy.clearCookie('PHPSESSID')
+    cy.clearCookie('mage-cache-sessid')
+  })
 })
