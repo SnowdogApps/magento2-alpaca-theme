1ce8dc8e8644a838cd2b0d1b431ec6b9e42c91bd
Ebonom Mfam <ebonom.mfam@snow.dog>
2019-08-19 15:48:24 +0200

    54485 - added add to cart tests

---
+++ .tests/cypress/integration/catalog/cart/add-to-cart-configurable.js
@@ -0,0 +1,40 @@
+/// <reference types="Cypress" />
+
+function pickRandomitem(items) {
+  return items[Math.floor(Math.random() * items.length)]
+}
+
+describe('CATALOG - Add to cart configurable test', () => {
+  it('Go to mens catalog view', () => {
+    cy.visit('https://alpaca-ce-demo.snowdog.pro/men.html')
+  })
+
+  it('Checks configurable options and adds to cart', () => {
+    cy.get('[data-testid=list-product-item]').first().focus()
+      .then(() => {
+        cy.get('.catalog-grid-item__options').should('be.visible').find('.size')
+        cy.get('.swatch-opt-1033').children('.swatch.size').find('.swatch__option')
+          .then((items) => {
+            cy.log(items)
+            pickRandomitem(items)
+              .click(items)
+          })
+        cy.get('.swatch-opt-1033').children('.swatch.color').find('.swatch__option')
+          .then((items) => {
+            cy.log(items)
+            pickRandomitem(items)
+              .click(items)
+          })
+        cy.get('.tocart').first().click()
+      })
+    cy.server()
+    cy.route('/customer/section/load/?sections=cart*').as('addToCart')
+    cy.wait('@addToCart')
+  })
+
+  it('Checks if mini-cart is not empty', () => {
+    cy.get('[data-testid=minicart-link]').click()
+    cy.get('#minicart-content-wrapper')
+    cy.contains('You have no items in your shopping cart.').should('not.be.visible')
+  })
+})
--- .tests/cypress/integration/catalog/cart/add-to-cart.js
+++ .tests/cypress/integration/catalog/cart/add-to-cart.js
@@ -1,10 +1,44 @@
+/// <reference types="Cypress" />
+
+function pickRandomitem(items) {
+  return items[Math.floor(Math.random() * items.length)]
+}
+
 describe('CATALOG - Add to cart test', () => {
   it('Go to catalog view', () => {
-    cy.select_first_menu()
+    cy.visit('men/tops-men')
   })
 
   it('click on add to cart button and redirect user to product page.', () => {
     cy.get('[data-testid="add-to-cart-button"]').first().trigger('mouseover').click()
-    cy.get('div[class="product-view container"]').should('be.visible')
+    cy.get('.product-view').should('be.visible')
+
+    cy.server()
+    cy.route('customer/section/load/?sections*').as('waitGallery')
+    cy.wait('@waitGallery')
+
+    //choose size
+    cy.get('.swatch-opt').should('be.visible').find('.size')
+    cy.get('.size').find('.swatch__option').then(items => {
+      pickRandomitem(items)
+        .click()
+    })
+
+    //choose color
+    cy.get('.color').find('.swatch__option').then(items => {
+      pickRandomitem(items)
+        .click()
+    })
+    //add to cart
+    cy.get('#product-addtocart-button').click()
+
+    cy.route('customer/section/load/?sections=cart*').as('addToCart')
+    cy.wait('@addToCart')
+  })
+
+  it('Check if mini-cart is not empty', () => {
+    cy.get('[data-testid=minicart-link]').click()
+    cy.get('#minicart-content-wrapper')
+    cy.contains('You have no items in your shopping cart.').should('not.be.visible')
   })
 })
