8c1a20f892732a97bbe3957c6437ccbf947dae81
Ania <anna.karon@snow.dog>
2019-09-02 13:57:06 +0200

    base configs

---
+++ Snowdog_Components/README.md
@@ -0,0 +1,87 @@
+[![Greenkeeper badge](https://badges.greenkeeper.io/SnowdogApps/magento2-alpaca-components.svg)](https://greenkeeper.io/)
+![Browser Status](https://badges.herokuapp.com/browsers?googlechrome=63,64,65,66,67&firefox=58,59&safari=11&iphone=11.2&android=64&iexplore=11&microsoftedge=16)
+
+## Introduction
+Components library of Alpaca [design system](https://www.uxpin.com/studio/blog/design-systems-vs-pattern-libraries-vs-style-guides-whats-difference/) created to speed up the process of working with design on Magento 2 stores, by creating each UI element, module, and view in a simplified, front-end developer friendly, environment.
+
+This components liblary is built on top of [Fractal.js](http://fractal.build/guide).
+
+Magento 2 experience isn't required to work with this code.
+
+
+## Installation
+- Install dependencies using `yarn`
+- Run `gulp fractal:start` to start Fractal.js development server
+- Run `gulp` if you want to generate static files (for example to deploy them)
+
+## How to create a components library on top of Colibri
+It's necessary only to work outside the Magento.
+1. Copy-paste `package.json`, `gulpfile.js`, `.eslintignore`, `.eslintrc`, `.sass-lint.yml`, `.stylelintrc`, `.gitignore` files to the new project
+2. Create `modules.json` file with an array of paths to parent components libraries.
+   In most cases it will look like this:
+   ```json
+   ["../../snowdog/module-colibri-components"]
+   ```
+3. Customize or add new files following the same structure as in Alpaca components library
+4. Run `gulp fractal:start`
+
+## Directory structure
+- `components` directory is what you are going to import into the Magento 2 theme.
+- `docs` and `public` are just for the local environment purposes, you will find there sample images, testing libs, utility styles etc.
+
+## Core concepts
+### Components architecture
+Components are divided into 4 groups:
+1. Globals
+   - Contains code that other components can use in any place, for example, typography, icons or SASS variables.
+2. Elements
+   - Smallest UI parts, for example, buttons
+   - Element can't depend on other elements
+   - Element shouldn't have any layout
+3. Modules
+   - More complex UI parts like search form or header
+   - Takes elements or modules and combine them together adding layout and context
+4. Views
+   - Takes elements or modules and combine them together adding final layout and context
+   - You should be able to show it to client/PM as a preview of ready to use store
+   - You shouldn't create any new UI elements, everything needs to be reusable.
+
+### Naming
+* Group name needs to be plural
+* Component name needs to be singular
+* Component name shouldn't be related to any project or place in the layout
+   - Bad: `filters`
+   - Good: `dropdown-list` or `collapsible-list`
+* Avoid using `box`, `block`, `item`, `info`, `text`, `cms`, especialy combined together, for example `info-box`
+* Avoid naming two components in similar way i.e. `cms-subcategories` and `cms-subcategory`
+
+### Colors
+* By default alpaca components uses up to 7 step grayscale
+
+### SASS Variables naming
+
+Follow BEM-like naming convention i.e. when you component name is `button` and you are creating a variable for a `padding` it should be `$button__padding`.
+
+Same as in BEM, you are not allowed to build construction like `$button__icon__padding`, it should be `$button__icon-padding`.
+
+Variables related to the pseudo-classes and pseudo-selectors should be treated as a BEM elements `$button__color-hover`.
+
+Also, BEM modificators are allowed in variables `$button__padding--secondary`, you can even stack them like this `$button__padding--secondary--dark`.
+
+To target variable to specific breakpoint adding `\@breakpoint` at the end of the variable name `$button__padding\@medium`. Always use `small`, `medium`, `large` etc. to describe the breakpoint.
+
+Examples of proper variables names:
+```scss
+$swatch__option-border
+$swatch__option-border-color-hover
+
+$swatch__option-image-height\@large
+
+$swatch__option-size--small
+$swatch__option-image-width--small
+$swatch__option-image-width--small\@large
+```
+
+## Accessibility
+Components are tested with [axe-core](https://github.com/dequelabs/axe-core). The results are displayed as a console output in browser tools.
+A11y test files you can find in `/public/tests/`
---
+++ Snowdog_Components/gulpfile.js
@@ -0,0 +1,295 @@
+/* eslint-env node */
+
+const autoprefixer = require('autoprefixer'),
+      bluebird     = require('bluebird'),
+      eslint       = require('gulp-eslint'),
+      fractal      = require('@frctl/fractal').create(),
+      fs           = require('fs-extra'),
+      globby       = require('globby'),
+      gulp         = require('gulp'),
+      gulpif       = require('gulp-if'),
+      hbsEngine    = fractal.components.engine(),
+      log          = require('gulp-logger'),
+      logger       = fractal.cli.console,
+      mandelbrot   = require('@frctl/mandelbrot'),
+      notify       = require('gulp-notify'),
+      path         = require('path'),
+      plumber      = require('gulp-plumber'),
+      postcss      = require('gulp-postcss'),
+      reporter     = require('postcss-reporter'),
+      runSequence  = require('run-sequence'),
+      sass         = require('gulp-sass'),
+      sassError    = require('gulp-sass-error'),
+      sassLint     = require('gulp-sass-lint'),
+      sourcemaps   = require('gulp-sourcemaps'),
+      stylelint    = require('stylelint'),
+      svgSprite    = require('gulp-svg-sprite'),
+      util         = require('gulp-util'),
+      a11yPreview  = '@views';
+
+// Turn off Bluebird unhandled promises warnings
+bluebird.config({
+  warnings: false
+});
+
+// Fractal configuration
+fractal.set('project.title', 'Colibri components for Magento 2');
+fractal.components.set('path', __dirname + '/build/components');
+fractal.docs.set('path', __dirname + '/build/docs');
+fractal.web.set('static.path', __dirname + '/build/public');
+fractal.web.set('builder.dest', __dirname + '/dest');
+fractal.web.theme(mandelbrot({ skin: 'black' }));
+
+// Handlebars helpers
+hbsEngine.handlebars.registerHelper('static', (file, data) => {
+  return '/components/raw/' + data.data.root._self.baseHandle + '/' + file;
+});
+
+hbsEngine.handlebars.registerHelper('svg', icon => {
+  return '/images/icons-sprite.svg#' + icon;
+});
+
+hbsEngine.handlebars.registerHelper('component', name => {
+  return '@' + name;
+});
+
+// Fractal gulp tasks
+gulp.task('fractal:start', ['a11y', 'inheritance', 'svg-sprite', 'sass', 'watch'], () => {
+  const server = fractal.web.server({
+    sync: true,
+    port: 4000
+  });
+
+  server.on('error', err => logger.error(err.message));
+
+  return server.start().then(() => {
+    logger.success(`Fractal server is now running at ${server.url}`);
+  });
+});
+
+
+gulp.task('fractal:build', ['inheritance', 'svg-sprite', 'sass'], () => {
+  const builder = fractal.web.builder();
+
+  if (!util.env.ci) {
+    builder.on('progress', (completed, total) => {
+      return logger.update(`Exported ${completed} of ${total} items`, 'info');
+    });
+  }
+
+  builder.on('error', err => logger.error(err.message));
+
+  return builder.build().then(() => {
+    logger.success('Fractal build completed!');
+  });
+});
+
+// Gulp tasks
+gulp.task('a11y', () => {
+  fractal.cli.command('a11y-preview', setA11yViewsPreview, a11yPreview);
+  return fractal.cli.exec('a11y-preview');
+  // fractal.components.set('default.preview', '@a11y-tests');
+  // runSequence('fractal:start');
+});
+
+function setA11yViewsPreview(args, done) {
+  const app = this.fractal;
+  for (let item of app.components.flatten()) {
+    if (item.viewPath.includes('04-views')) {
+      item.preview = a11yPreview;
+    }
+  }
+  done();
+}
+
+const a11y = () => {
+  fractal.cli.command('a11y-preview', setA11yViewsPreview, a11yPreview);
+  return fractal.cli.exec('a11y-preview');
+}
+
+gulp.task('watch', () => {
+  gulp.watch([
+    fractal.components.get('path') + '/**/*.scss',
+    fractal.docs.get('path') + '/styles/**/*.scss'
+  ], () => {
+    runSequence('sass-lint', 'sass', 'css-lint');
+  });
+
+  gulp.watch(fractal.components.get('path') + '/**/*.js', () => {
+    runSequence('js-lint');
+  });
+
+  gulp.watch(fractal.components.get('path') + '/01-globals/icons/files/*.svg', () => {
+    runSequence('svg-sprite');
+  });
+});
+
+gulp.task('sass', () => {
+  return gulp.src(fractal.docs.get('path') + '/styles/**/*.scss')
+    .pipe(
+      gulpif(
+        util.env.ci,
+        log({
+          display: 'name',
+          beforeEach: 'Processing: '
+        })
+      )
+    )
+    .pipe(
+      gulpif(!util.env.ci,
+        plumber({
+          errorHandler: notify.onError('Error: <%= error.message %>')
+        })
+      )
+    )
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sassError.gulpSassError(util.env.ci || false)))
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write('cssmaps'))
+    .pipe(gulp.dest(fractal.web.get('static.path') + '/css'));
+});
+
+gulp.task('sass-lint', () => {
+  return gulp.src(fractal.components.get('path') + '/**/*.scss')
+    .pipe(
+      gulpif(
+        util.env.ci,
+        log({
+          display: 'name',
+          beforeEach: 'Processing: '
+        })
+      )
+    )
+    .pipe(
+      gulpif(!util.env.ci,
+        plumber({
+          errorHandler: notify.onError('Error: <%= error.message %>')
+        })
+      )
+    )
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(gulpif(util.env.ci, sassLint.failOnError()));
+});
+
+gulp.task('css-lint', () => {
+  return gulp.src(fractal.web.get('static.path') + '/**/*.css')
+    .pipe(
+      gulpif(
+        util.env.ci,
+        log({
+          display: 'name',
+          beforeEach: 'Processing: '
+        })
+      )
+    )
+    .pipe(
+      gulpif(!util.env.ci,
+        plumber({
+          errorHandler: notify.onError('Error: <%= error.message %>')
+        })
+      )
+    )
+    .pipe(postcss([
+      stylelint(),
+      reporter({ throwError: util.env.ci || false })
+    ]));
+});
+
+gulp.task('js-lint', () => {
+  return gulp.src(fractal.components.get('path') + '/**/*.js')
+    .pipe(
+      gulpif(
+        util.env.ci,
+        log({
+          display: 'name',
+          beforeEach: 'Processing: '
+        })
+      )
+    )
+    .pipe(
+      gulpif(!util.env.ci,
+        plumber({
+          errorHandler: notify.onError('Error: <%= error.message %>')
+        })
+      )
+    )
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(gulpif(util.env.ci, eslint.failAfterError()));
+});
+
+gulp.task('svg-sprite', () => {
+  return gulp.src(fractal.components.get('path') + '/01-globals/icons/files/*.svg')
+    .pipe(svgSprite({
+      mode: {
+        symbol: {
+          dest: 'images',
+          sprite: 'icons-sprite.svg'
+        }
+      }
+    }))
+    .pipe(gulp.dest(fractal.web.get('static.path')));
+});
+
+gulp.task('inheritance', done => {
+  const components = fractal.components.get('path').replace(__dirname + '/build/', ''),
+        docs = fractal.docs.get('path').replace(__dirname + '/build/', ''),
+        static = fractal.web.get('static.path').replace(__dirname + '/build/', '');
+
+  // Remove old build directory
+  fs.removeSync('./build');
+
+  // Find all local files
+  globby
+    .sync([
+      components + '/**',
+      docs + '/**',
+      static + '/**'
+    ], { nodir: true })
+    .forEach(file => {
+      // Symlink all local files to build dir
+      if (util.env.ci) {
+        fs.copySync(
+          file,
+          'build/' + file
+        );
+      }
+      else {
+        fs.ensureSymlinkSync(
+          file,
+          'build/' + file
+        );
+      }
+    });
+  if (fs.existsSync('./modules.json')) {
+    const modules = require('./modules.json.js');
+
+    // Go through array of module paths
+    modules.forEach(src => {
+      src = path.resolve(src);
+
+      // Find all module files
+      globby
+        .sync([
+          src + '/' + components + '/**',
+          src + '/' + docs + '/**',
+          src + '/' + static + '/**'
+        ], { nodir: true })
+        .forEach(file => {
+          const srcPath = path.resolve(file),
+                destPath = srcPath.replace(src, path.resolve('build'));
+
+          // Symlink all module files to build dir
+          if (util.env.ci) {
+            fs.copySync(srcPath, destPath, { overwrite: false });
+          }
+          else {
+            fs.ensureSymlinkSync(srcPath, destPath);
+          }
+        });
+    });
+  }
+  done();
+});
