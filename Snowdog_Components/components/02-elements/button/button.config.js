fe1fe293b6217c526863f183fc19553d09300dca
Ania <anna.karon@snow.dog>
2019-09-02 14:02:32 +0200

    button & loader components

---
+++ Snowdog_Components/components/02-elements/button/button.config.js
@@ -0,0 +1,95 @@
+module.exports = {
+  preview: '@docs-only-styles',
+  collated: true,
+  context: {
+    tag: 'button',
+    class: '',
+    attributes: 'type="button" aria-label="button"',
+    text: 'I am a button'
+  },
+  variants: [
+    {
+      name: 'icon',
+      context: {
+        text: '',
+        icon: {
+          id: 'arrow-left',
+          title: 'Arrow left',
+          class: 'button__icon',
+        },
+        attributes: 'type="button" aria-label="click to do something"'
+      }
+    },
+    {
+      name: 'add-to-icon',
+      view: 'button--icon.hbs',
+      context: {
+        text: '',
+        class: 'button--add-to-icon',
+        icon: {
+          id: 'shopping-cart',
+          title: 'Add to Cart',
+          class: 'button__icon',
+        },
+        attributes: 'type="button" aria-label="click to do something"'
+      }
+    },
+    {
+      name: 'secondary',
+      context: {
+        class: 'button--secondary',
+        text: 'I am button secondary'
+      }
+    },
+    {
+      name: 'secondary-light',
+      context: {
+        class: 'button--secondary-light',
+        text: 'I am button secondary light'
+      }
+    },
+    {
+      name: 'link',
+      context: {
+        tag: 'a',
+        class: 'button--link',
+        attributes: 'href="#" title="I am a link button"',
+        text: 'I am a link button'
+      }
+    },
+    {
+      name: 'fluid',
+      context: {
+        class: 'button--fluid'
+      }
+    },
+    {
+      name: 'icon-before',
+      view: 'button--icon.hbs',
+      context: {
+        text: 'Button Icon before',
+        iconBefore: {
+          id: 'arrow-left',
+          title: 'Arrow left',
+          class: 'button__icon',
+          hidden: true
+        },
+        attributes: 'type="button" aria-label="click to do something"'
+      }
+    },
+    {
+      name: 'icon-after',
+      view: 'button--icon.hbs',
+      context: {
+        text: 'Button Icon after',
+        iconAfter: {
+          id: 'arrow-right',
+          title: 'Arrow Right',
+          class: 'button__icon',
+          hidden: true
+        },
+        attributes: 'type="button" aria-label="click to do something"'
+      }
+    },
+  ]
+};
---
+++ Snowdog_Components/components/02-elements/loader/loader.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Loader',
+  context: {},
+  variants: [
+    {
+      name: 'inline',
+      context: {}
+    }
+  ]
+}
