0133a8d35f07500bfa67a7ae2db899c5acd2e049
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2019-09-04 14:02:14 +0200

    #57528 - copy breadcrumbs component

---
+++ Snowdog_Components/components/Molecules/breadcrumbs/breadcrumbs.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  context: {
+    ariaLabel: 'Breadcrumbs',
+    categories: [
+      {
+        name: 'Home',
+        href: '#',
+        linkClass: 'breadcrumbs__link link--invert'
+      },
+      {
+        name: 'category2',
+        href: '#',
+        linkClass: 'breadcrumbs__link link--invert'
+      },
+      {
+        name: 'category3',
+        href: '#',
+        linkClass: 'breadcrumbs__link link--invert'
+      },
+      {
+        name: 'category4',
+        href: '#',
+        linkClass: 'breadcrumbs__link link--invert'
+      },
+      {
+        name: 'category5',
+        href: '#',
+        linkClass: 'breadcrumbs__link link--invert'
+      },
+      {
+        name: 'category6',
+        href: '#',
+      }
+    ]
+  }
+};
