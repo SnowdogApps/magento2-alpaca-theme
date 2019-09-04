503c5df6ac3625681a4e254d7e065811fd7589ed
Ania <anna.karon@snow.dog>
2019-09-04 17:06:28 +0200

    57562 select component

---
+++ Snowdog_Components/components/Molecules/form/select/select.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  context: {
+    label: {
+      attributes: '',
+      text: 'Label text'
+    },
+    field: {
+      id: 'field-id',
+      name: 'field-name',
+      class: '',
+      attributes: ''
+    },
+    options: [
+      {
+        value: 'option1',
+        text: 'Option 1'
+      },
+      {
+        value: 'option2',
+        text: 'Option 2'
+      },
+      {
+        value: 'option3',
+        text: 'Option 3',
+        attributes: 'disabled'
+      }
+    ]
+  }
+};
