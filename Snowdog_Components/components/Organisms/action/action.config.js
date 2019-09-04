6c272e86d50cd822dbb955162f22c66ded2b81b5
Jarek Krupiński <jarek.krupinski@snow.dog>
2019-09-04 15:32:04 +0200

    #57502 - Update action component styles and config

--- Snowdog_Components/components/Organisms/action/action.config.js
+++ Snowdog_Components/components/Organisms/action/action.config.js
@@ -7,11 +7,26 @@ module.exports = {
             button: {
               class: 'action__button'
             }
+          },
+          {
+            button: {
+              class: 'action__button'
+            }
           }
         ]
       },
       {
         action: [
+          {
+            link: {
+              class: 'action__link'
+            }
+          },
+          {
+            link: {
+              class: 'action__link'
+            }
+          },
           {
             link: {
               class: 'action__link'
