abcf2e86740b2fffcca0cb0ad97ad50307831bea
Ania <anna.karon@snow.dog>
2019-09-04 12:40:47 +0200

    57562 file upload component

--- Snowdog_Components/components/Molecules/form/checkbox/checkbox.config.js
+++ Snowdog_Components/components/Molecules/form/checkbox/checkbox.config.js
@@ -19,6 +19,7 @@ module.exports = {
       name: 'long-label',
       context: {
         id: 'checkbox-long-label',
+        name: 'checkbox-long-label-name',
         label: {
           text: 'I hereby agree for processing my personal data, included in my job offer, for the purpose of recruitment (as defined in the Act of August 29, 1997 on the Protection of Personal Data (Journal of Laws No. 133, item 883).'
         },
