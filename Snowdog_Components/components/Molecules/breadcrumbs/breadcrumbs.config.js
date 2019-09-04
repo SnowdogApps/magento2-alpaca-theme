4da884ae925768095795fad55ff552f4b518a910
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2019-09-04 14:39:40 +0200

    #57528 - breadcrumbs component styles + fixes

--- Snowdog_Components/components/Molecules/breadcrumbs/breadcrumbs.config.js
+++ Snowdog_Components/components/Molecules/breadcrumbs/breadcrumbs.config.js
@@ -5,27 +5,27 @@ module.exports = {
       {
         name: 'Home',
         href: '#',
-        linkClass: 'breadcrumbs__link link--invert'
+        linkClass: 'breadcrumbs__link'
       },
       {
         name: 'category2',
         href: '#',
-        linkClass: 'breadcrumbs__link link--invert'
+        linkClass: 'breadcrumbs__link'
       },
       {
         name: 'category3',
         href: '#',
-        linkClass: 'breadcrumbs__link link--invert'
+        linkClass: 'breadcrumbs__link'
       },
       {
         name: 'category4',
         href: '#',
-        linkClass: 'breadcrumbs__link link--invert'
+        linkClass: 'breadcrumbs__link'
       },
       {
         name: 'category5',
         href: '#',
-        linkClass: 'breadcrumbs__link link--invert'
+        linkClass: 'breadcrumbs__link'
       },
       {
         name: 'category6',
