a5f230440f05e502e034d40a377b015eb9dd9702
Ania <anna.karon@snow.dog>
2019-09-05 18:39:59 +0200

    57917 header - base mobile structure

---
+++ Snowdog_Components/components/Organisms/header/header.config.js
@@ -0,0 +1,66 @@
+module.exports = {
+  title: 'Header module',
+  context: {
+    buttons: [
+      {
+        ariaLabel: 'Wishlist',
+        class: 'header-button--reset-margin',
+        icon: {
+          id: 'heart',
+          title: 'Heart',
+          class: 'header-button__icon'
+        }
+      }
+    ],
+    logo: {
+      class: 'header__logo',
+      logoSVG: true,
+      ariaLabel: 'Home page link',
+      imageAlt: 'Alpaca Logo',
+      imageAttributes: '',
+      imageClass: '',
+      imageSrc: '/images/logo/logo.svg',
+      href: '#'
+    },
+    searchInput: {
+      class: 'search-form__input-wrapper',
+      label: {
+        hidden: true
+      },
+      field: {
+        class: 'search-form__input',
+        id: 'search',
+        name: 'search',
+        type: 'search',
+        placeholder: 'Search entire store here...'
+      }
+    },
+    searchButton: {
+      tag: 'button',
+      text: '',
+      class: 'button--icon search-form__button',
+      icon: {
+        id: 'search',
+        title: 'Search',
+        class: 'button__icon'
+      },
+      attributes: 'type="button" aria-label="Search"',
+    },
+    minicartTrigger: {
+      icon: {
+        id: 'shopping-cart',
+        title: 'Shopping cart',
+        class: 'header-button__icon'
+      },
+      link: '/cart',
+      ariaLabel: 'Shopping cart dropdown trigger'
+    },
+    minicartPopup: {
+      class: 'header__minicart-content',
+      popupId: 'popup-minicart',
+      popupTrigger: false,
+      content: 'minicart-content',
+      script: true
+    }
+  }
+}
---
+++ Snowdog_Components/components/Organisms/side-menu/side-menu.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class SideMenu {
+  constructor() {
+    this.sideMenuContainer = document.querySelector('.side-menu');
+    this.sideMenuOverlay = this.sideMenuContainer.querySelector('.side-menu__overlay');
+    this.menuTrigger = this.sideMenuContainer.querySelectorAll('.side-menu__trigger');
+    this.init();
+  }
+
+  toggleSideMenu() {
+    this.sideMenuContainer.classList.toggle('side-menu--is-open');
+
+    if (this.sideMenuContainer.classList.contains('side-menu--is-open')) {
+      document.body.style.overflow = 'hidden';
+    }
+    else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+
+  setListeners() {
+    this.menuTrigger.forEach(el => {
+      el.addEventListener('click', () => {
+        this.toggleSideMenu();
+      });
+    });
+
+    this.sideMenuOverlay.addEventListener('click', () => {
+      this.toggleSideMenu();
+    });
+  }
+
+  init() {
+    this.setListeners();
+  }
+}
+
+new SideMenu();
