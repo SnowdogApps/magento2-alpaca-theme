ee85c21c8e20e804fc91f90d337b63b5a27db5c0
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2019-09-05 14:35:56 +0200

    #57537 - copy contact bar component

---
+++ Snowdog_Components/components/Organisms/contact-bar/contact-bar.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  context: {
+    class: '',
+    attributes: '"',
+    phone: {
+      icon: {
+        class: '',
+        iconId: 'phone',
+        iconTitle: 'Phone'
+      },
+      text: 'Call us toll free',
+      number: '800 - 266 - 9590'
+    },
+    email: {
+      icon: {
+        class: '',
+        iconId: 'mail',
+        iconTitle: 'Email'
+      },
+      text: 'Email Us',
+      address: 'test@test.com'
+    },
+    menu: {
+      items: [
+        {
+          text: 'Telescope User Guide',
+          link: '#'
+        },
+        {
+          text: 'Account',
+          link: '#'
+        },
+        {
+          text: 'Blog',
+          link: '#'
+        },
+        {
+          text: 'Contact',
+          link: '#'
+        }
+      ]
+    }
+  }
+};
