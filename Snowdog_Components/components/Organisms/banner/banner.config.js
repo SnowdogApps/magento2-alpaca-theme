273d59185353b90c93d52845b02347677e6c2039
Jarek Krupiński <jarek.krupinski@snow.dog>
2019-09-06 13:09:25 +0200

    #57503 - Add banner--category

--- Snowdog_Components/components/Organisms/banner/banner.config.js
+++ Snowdog_Components/components/Organisms/banner/banner.config.js
@@ -23,6 +23,12 @@ module.exports = {
         }
       ],
       alt: 'banner-image'
+    },
+    imageCategory: {
+      class: 'banner__image',
+      src: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+      dataSrc: '/images/banner/banner-1920_512.jpg',
+      alt: 'image alt text'
     }
   },
   variants: [
@@ -35,6 +41,39 @@ module.exports = {
           title: 'Arrow right icon'
         },
         text: 'Clothing'
+      }
+    },
+    {
+      name: 'category',
+      view: 'banner--category.hbs',
+      context: {
+        buttonCollapse: {
+          tag: 'button',
+          class: 'banner__button banner__button-collapse',
+          attributes: 'type="button" aria-label="button"',
+          text: 'More about test',
+          iconAfter: {
+            id: 'angle-down'
+          }
+        },
+        buttonClose: {
+          tag: 'button',
+          class: 'banner__button banner__button-close margin-vc-xs',
+          attributes: 'type="button" aria-label="button"',
+          text: 'Hide details',
+          iconAfter: {
+            id: 'angle-up'
+          }
+        },
+        buttonMore: {
+          tag: 'a',
+          class: 'banner__button banner__button-more margin-vc-xs',
+          attributes: 'aria-label="button" tabindex="0"',
+          text: 'View All',
+          iconAfter: {
+            id: 'arrow-right'
+          }
+        }
       }
     }
   ]
