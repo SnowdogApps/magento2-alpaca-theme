6a8564c4ba346acc8ba69e803b9b6b162f3334be
Ebonom Mfam <ebonom.mfam@snow.dog>
2019-09-17 15:48:15 +0200

    #54485 - removed account from custom command since it's used only once.

--- .tests/cypress/integration/header/guest/account.js
+++ .tests/cypress/integration/header/guest/account.js
@@ -1,5 +1,6 @@
 describe('HEADER - Account header link as Guest user', () => {
   it('redirects guest user to customer login page.', () => {
-    cy.account()
+    cy.visit('/customer/account/')
+    cy.get('.login__form').should('be.visible')
   })
 })
--- .tests/cypress/support/commands.js
+++ .tests/cypress/support/commands.js
@@ -1,8 +1,3 @@
-Cypress.Commands.add('account', () => {
-  cy.visit('/customer/account/')
-  cy.get('.login__form').should('be.visible')
-})
-
 Cypress.Commands.add('login', () => {
   cy.visit('/customer/account/login/')
   cy.get('input[name="form_key"]')
