44406eec62344912cdd58d2ec0ed18a1d0d7a273
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2019-10-03 14:21:17 +0200

    #59131 - not found styles

---
+++ Snowdog_Components/components/Templates/404/404.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  context: {
+    homeLink: {
+      class: 'button--secondary not-found__link',
+      attributes: 'href="#" title="Store Home"',
+      text: 'Store Home'
+    },
+    accountLink: {
+      class: 'not-found__link',
+      attributes: 'href="#" title="My account"',
+      text: 'My account'
+    }
+  }
+}
