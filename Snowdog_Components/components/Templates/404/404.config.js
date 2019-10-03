158c3600d5bd2e649480534d55361921acb15927
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2019-10-03 14:23:58 +0200

    #59131 - config setup

--- Snowdog_Components/components/Templates/404/404.config.js
+++ Snowdog_Components/components/Templates/404/404.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   context: {
+    error: '404',
+    heading: 'Whoops, our bad...',
     homeLink: {
       class: 'button--secondary not-found__link',
       attributes: 'href="#" title="Store Home"',
