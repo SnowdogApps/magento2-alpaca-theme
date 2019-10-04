deee480242d38386fca148a90f053816048b786c
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2019-10-04 11:38:07 +0200

    #59131 - cr fix - formatting

--- Snowdog_Components/components/Templates/404/404.config.js
+++ Snowdog_Components/components/Templates/404/404.config.js
@@ -11,6 +11,7 @@ module.exports = {
       class: 'not-found__link',
       attributes: 'href="#" title="My account"',
       text: 'My account'
-    }
+    },
+    text: '<p>The page you requested was not found, and we have a fine guess why.<br>If you typed the URL directly, please make sure the spelling is correct.<br>If you clicked on a link to get here, the link is outdated.</p><p>What can you do?<br>Have no fear, help is near! There are many ways you can get back on track with Magento Store.</p><p>Go back to the previous page.<br>Use the search bar at the top of the page to search for your products.<br>Follow these links to get you back on track!</p>'
   }
 }
