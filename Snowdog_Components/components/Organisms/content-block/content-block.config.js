026dfb1d4cc14ca1924af55ea2bf1b3f638ecfff
Karol Osuchowski <karol.osuchowski@snow.dog>
2019-10-09 23:05:27 +0200

    #57955 - fix indentations

--- Snowdog_Components/components/Organisms/content-block/content-block.config.js
+++ Snowdog_Components/components/Organisms/content-block/content-block.config.js
@@ -31,46 +31,46 @@ module.exports = {
     ]
   },
   variants: [
-     {
-       name: '2-columns',
-       context: {
-         class: 'content-block--2-columns',
-         columns: [
-           {
-             class: 'col-xs-12 col-md-4',
-             elements: [
-               {
-                 contentElement: 'heading',
-                 contentContext: {
-                   tag: 'h2',
-                   class: 'content-block__heading  content-block__heading--secondary',
-                   text: 'Lorem ipsum is simply'
-                 }
-               }
-             ]
-           },
-           {
-             class: 'col-xs-12 col-md-8',
-             elements: [
-               {
-                 contentElement: 'paragraph',
-                 contentContext: {
-                   tag: 'span',
-                   class: 'content-block__introduction',
-                   text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s'
+    {
+      name: '2-columns',
+      context: {
+        class: 'content-block--2-columns',
+        columns: [
+          {
+            class: 'col-xs-12 col-md-4',
+            elements: [
+              {
+                contentElement: 'heading',
+                contentContext: {
+                  tag: 'h2',
+                  class: 'content-block__heading  content-block__heading--secondary',
+                  text: 'Lorem ipsum is simply'
+                }
+              }
+            ]
+          },
+          {
+            class: 'col-xs-12 col-md-8',
+            elements: [
+              {
+                contentElement: 'paragraph',
+                contentContext: {
+                  tag: 'span',
+                  class: 'content-block__introduction',
+                  text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s'
 
-                 }
-               },
-               {
-                 contentElement: 'paragraph',
-                 contentContext: {
-                   tag: 'span',
-                   class: 'content-block__description'
-                 }
-               }
-             ]
-           }
-         ]
+                }
+              },
+              {
+                contentElement: 'paragraph',
+                contentContext: {
+                  tag: 'span',
+                  class: 'content-block__description'
+                }
+              }
+            ]
+          }
+        ]
       }
     }
   ]
