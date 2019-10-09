9696c0831b0c7d0cffd6bd230cece9f696c60e7b
Ania <anna.karon@snow.dog>
2019-10-09 12:43:00 +0200

    59254 update contact-bar variant

--- Snowdog_Components/components/Organisms/contact-bar/contact-bar.config.js
+++ Snowdog_Components/components/Organisms/contact-bar/contact-bar.config.js
@@ -39,9 +39,9 @@ module.exports = {
   },
   variants: [
     {
-      name: 'side-menu',
+      name: 'vertical',
       context: {
-        class: 'contact-bar--side-menu'
+        class: 'contact-bar--vertical'
       }
     }
   ]
--- Snowdog_Components/components/Organisms/side-menu/side-menu.config.js
+++ Snowdog_Components/components/Organisms/side-menu/side-menu.config.js
@@ -25,7 +25,7 @@ module.exports = {
       modalBottom: true,
       modalBottomActions: [
         {
-          content: 'contact-bar--side-menu'
+          content: 'contact-bar--vertical'
         }
       ],
       buttonClose: {
