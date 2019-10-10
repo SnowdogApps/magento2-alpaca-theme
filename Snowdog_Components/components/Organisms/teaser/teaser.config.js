9ed83545892398445b38aa021eb67e476f059418
Karol Osuchowski <karol.osuchowski@snow.dog>
2019-10-10 07:35:48 +0200

    #57955 - refactor teaser configuration and styles

--- Snowdog_Components/components/Organisms/teaser/teaser.config.js
+++ Snowdog_Components/components/Organisms/teaser/teaser.config.js
@@ -1,8 +1,25 @@
 module.exports = {
   context: {
-    class: 'teaser--right',
+    class: 'teaser-left',
     image: {
-
+      src: '/images/home/teaser/teaser-small-girl_304-304.jpg',
+      wrapperClass: 'teaser__image',
+      defaultSrc: '/images/home/teaser/teaser-small-girl_304-304.jpg',
+      sources: [
+        {
+          src: '/images/home/teaser/teaser-medium-girl_376-432.jpg',
+          mediaQuery: '(max-width: 768px)'
+        },
+        {
+          src: '/images/home/teaser/teaser-medium-girl_472-432.jpg',
+          mediaQuery: '(max-width: 1024px)'
+        },
+        {
+          src: '/images/home/teaser/teaser-large-girl_1328-1200.jpg',
+          mediaQuery: ''
+        }
+      ],
+      alt: 'Teaser girl image'
     },
     contentElement: 'content-block'
   }
