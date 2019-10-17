90607e78b7212bf3d4391fa7b87e729739fd59bc
Karol Osuchowski <karol.osuchowski@snow.dog>
2019-10-17 15:22:53 +0200

    #57955 - add missing padding for blocks, simplify structure and add missing config for banners

--- Snowdog_Components/components/Templates/home/home.config.js
+++ Snowdog_Components/components/Templates/home/home.config.js
@@ -2,22 +2,93 @@ module.exports = {
   context: {
     categoriesHeading: {
       tag: 'h2',
-      class: 'home__cms-block-title',
+      class: 'home__heading',
       text: 'Shop by category'
+    },
+    clothingBanner: {
+      text: 'Clothing',
+      class: 'col-xs-6 col-lg-3 home__category-banner',
+      image: {
+        defaultSrc: '/images/home/banners/category-clothing-144_200.jpg',
+        sources: [
+          {
+            src: '/images/home/banners/category-clothing-320_432.jpg',
+            mediaQuery: '(max-width: 1200px)'
+          },
+          {
+            src: '/images/home/banners/category-clothing-320_432.jpg',
+            mediaQuery: ''
+          }
+        ]
+      }
+    },
+    shoesBanner: {
+      text: 'Shoes',
+      class: 'col-xs-6 col-lg-3 home__category-banner',
+      image: {
+        defaultSrc: '/images/home/banners/category-shoes-144_200.jpg',
+        sources: [
+          {
+            src: '/images/home/banners/category-shoes-320_432.jpg',
+            mediaQuery: '(max-width: 1200px)'
+          },
+          {
+            src: '/images/home/banners/category-shoes-320_432.jpg',
+            mediaQuery: ''
+          }
+        ]
+      }
+    },
+    accessoriesBanner: {
+      text: 'Accessories',
+      class: 'col-xs-6 col-lg-3 home__category-banner',
+      image: {
+        defaultSrc: '/images/home/banners/category-accessories-144_200.jpg',
+        sources: [
+          {
+            src: '/images/home/banners/category-accessories-320_432.jpg',
+            mediaQuery: '(max-width: 1200px)'
+          },
+          {
+            src: '/images/home/banners/category-accessories-320_432.jpg',
+            mediaQuery: ''
+          }
+        ]
+      }
+    },
+    newInBanner: {
+      text: 'New In',
+      class: 'col-xs-6 col-lg-3 home__category-banner',
+      image: {
+        defaultSrc: '/images/home/banners/category-new-in-144_200.jpg',
+        sources: [
+          {
+            src: '/images/home/banners/category-new-in-320_432.jpg',
+            mediaQuery: '(max-width: 1200px)'
+          },
+          {
+            src: '/images/home/banners/category-new-in-320_432.jpg',
+            mediaQuery: ''
+          }
+        ]
+      }
+    },
+    banner: {
+      class: 'col-xs-12 col-lg-6 home__banner'
     },
     popularProductsHeading: {
       tag: 'h2',
-      class: 'home__cms-block-title',
+      class: 'home__heading',
       text: 'Popular products'
     },
     newProductsHeading: {
       tag: 'h2',
-      class: 'home__cms-block-title',
+      class: 'home__heading',
       text: 'New products'
     },
     brandsHeading: {
       tag: 'h2',
-      class: 'home__cms-block-title',
+      class: 'home__heading',
       text: 'Top brands'
     }
   }
