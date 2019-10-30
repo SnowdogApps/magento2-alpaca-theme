2dd4e410e9b91f546ee46ced16b02bec44d6e635
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2019-10-30 12:50:21 +0100

    #57525 - catalog banner

--- Magento_Theme/requirejs-config.js
+++ Magento_Theme/requirejs-config.js
@@ -6,6 +6,7 @@ var config = {
       "lazysizes"      : "Magento_Theme/js/lib/lazysizes",
       "picturefill"    : "Magento_Theme/js/lib/picturefill",
       "svg4everybody"  : "Magento_Theme/js/lib/svg4everybody",
+      "banner"         : "Magento_Theme/js/banner.babel",
       "cookieMessage"  : "Magento_Theme/js/cookie-message.babel",
       "dropdownList"   : "Magento_Theme/js/dropdown-list.babel",
       "marketingBar"   : "Magento_Theme/js/marketing-bar.babel",
