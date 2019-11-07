090648e3a011abd3e11b898edbc0199b2cfd3ff3
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2019-11-07 16:57:07 +0100

    #57525 - add moreInfo js

--- Smile_ElasticsuiteCatalog/requirejs-config.js
+++ Smile_ElasticsuiteCatalog/requirejs-config.js
@@ -2,7 +2,8 @@ var config = {
   map: {
     '*': {
       'priceRange': 'Smile_ElasticsuiteCatalog/js/price-range.babel',
-      'range'     : 'Smile_ElasticsuiteCatalog/js/range.babel'
+      'range'     : 'Smile_ElasticsuiteCatalog/js/range.babel',
+      'moreInfo'  : "Smile_ElasticsuiteCatalog/js/more-info.babel"
     }
   }
 };
