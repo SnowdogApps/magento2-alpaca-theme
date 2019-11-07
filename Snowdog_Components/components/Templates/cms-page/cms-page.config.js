9ee00d588bbe155cac62cfd9fa494049bff4c02e
Karol Osuchowski <karol.osuchowski@snow.dog>
2019-11-07 16:10:56 +0100

    #57973 - add cms page template with config

---
+++ Snowdog_Components/components/Templates/cms-page/cms-page.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  context: {
+    breadcrumbs: {
+      categories: [
+        {
+          name: 'Home',
+          href: '#',
+          linkClass: 'breadcrumbs__link'
+        },
+        {
+          name: 'About Us',
+          href: '#',
+          linkClass: 'breadcrumbs__link'
+        }
+      ]
+    },
+    mainHeading: {
+      text: 'About us',
+      class: 'margin-top-xl text-center'
+    }
+  }
+}
