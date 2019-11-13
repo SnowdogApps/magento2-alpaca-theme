ad06bc433eb5b8c764ca128168ffbddccc7a0a7b
Ania <anna.karon@snow.dog>
2019-11-13 16:45:44 +0100

    57974 update styles and script files - formatting, vars

--- Magento_Catalog/web/js/add-to-cart-more.babel.js
+++ Magento_Catalog/web/js/add-to-cart-more.babel.js
@@ -3,7 +3,7 @@ const mediaBq = window.matchMedia('(max-width: 767px)');
 
 function toggleBtnAction(button) {
   let currentState = button.getAttribute('aria-expanded'),
-      newState = currentState === 'true' ? 'false'  : 'true';
+      newState = currentState === 'true' ? 'false' : 'true';
   button.setAttribute('aria-expanded', newState);
 }
 
