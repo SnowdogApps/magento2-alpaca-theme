3290a3ff3514bbe3c655b2993b68a8c4a2b0e81a
Karol Osuchowski <karol.osuchowski@snow.dog>
2019-11-28 13:55:21 +0100

    #60507 - working on quicksearch - WIP

--- Smile_ElasticsuiteCore/web/js/form-mini-mixin.js
+++ Smile_ElasticsuiteCore/web/js/form-mini-mixin.js
@@ -22,19 +22,45 @@ define([
        * @private
        */
       _getResultWrapper: function () {
-        return $('<div class="quicksearch__results"></div>');
+        console.log((this.options))
+        return $('<div class="quicksearch__content"></div>');
       },
 
       _getSectionHeader: function(type, data) {
         var title = '';
-        var header = $('<div class="quicksearch__results-heading margin-bottom-0"></div>');
+        var product__header = $('<div class="quicksearch__list quicksearch__list--column"></div>');
+        var sidebar__header = $('<div class="quicksearch__sidebar-column"></div>');
 
-        if (type !== undefined) {
+        if (type !== undefined && type !== 'product') {
           title = this._getSectionTitle(type, data);
-          header.append(title);
+          sidebar__header.append(title);
+          return sidebar__header
         }
 
-        return header;
+        return product__header;
+      },
+
+      _initTitleRenderer: function() {
+        this.titleRenderers = {};
+        for (var typeIdentifier in this.options.templates) {
+          if (typeIdentifier !== 'product' && this.options.templates[typeIdentifier]['titleRenderer']) {
+            require([this.options.templates[typeIdentifier]['titleRenderer']], function (renderer) {
+              this.component.titleRenderers[this.type] = renderer;
+            }.bind({component: this, type: typeIdentifier}));
+          }
+        }
+      },
+
+      _getSectionTitle: function(type, data) {
+        var title = '';
+
+        if (this.titleRenderers && this.titleRenderers[type]) {
+          title = $('<h3 class="quicksearch__sidebar-title title-' + type + '">' + this.titleRenderers[type].render(data) + '</h3>');
+        } else if (this.options.templates && this.options.templates[type].title) {
+          title = $('<h3 class="quicksearch__sidebar-title title-' + type + '">' + this.options.templates[type].title + '</h3>');
+        }
+
+        return title;
       },
 
     });
