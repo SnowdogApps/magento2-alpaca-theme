b72cc6f8cfb8f16cf0bbe2cf0500605114eb52f4
Bartosz Dominiak <bartosz.dominiak@snow.dog>
2019-12-01 22:24:18 +0100

    #57967 - rewrited shipping-form to components

--- Snowdog_Components/components/Organisms/checkout/shipping-address/shipping-address.config.js
+++ Snowdog_Components/components/Organisms/checkout/shipping-address/shipping-address.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  preview: '@checkout'
+  preview: '@checkout',
+  context: {
+    labels: {
+      mail: 'Email Address',
+      passwords: 'Password'
+    },
+    notes: {
+      customer: 'You already have an account with us. Sign in or continue as guest.',
+      accounts: 'You can create an account after checkout.',
+    },
+    loginBtn: {
+      tag: 'button',
+      class: 'action login primary shipping-address__btn-login',
+      attributes: 'type="button" aria-label="button"',
+      text: 'Login'
+    },
+    forgotBtn: {
+      tag: 'a',
+      class: 'action remind shipping-address__btn-remind',
+      text: 'Forgot Your Password?'
+    },
+    questionIcon: {
+      id: 'question-mark'
+    }
+  }
 }
