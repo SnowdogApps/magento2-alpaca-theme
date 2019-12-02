15c721c74ce0f830584250e7b7537a2c3d27af91
Ania <anna.karon@snow.dog>
2019-12-02 17:38:20 +0100

    61605 add product to wishlist with ajax on catalog page

--- Magento_Wishlist/web/js/wishlist-item.js
+++ Magento_Wishlist/web/js/wishlist-item.js
@@ -1,6 +1,7 @@
 define([
   'jquery',
   'Magento_Customer/js/customer-data',
+  'mage/cookies',
   'domReady'
 ], function ($, customerData) {
   'use strict';
@@ -10,6 +11,35 @@ define([
       wishlist = customerData.get('wishlist'),
       items = wishlist().items,
       productId = config.productId;
+
+    button.on('click', function() {
+      const url = config.addToParams.action,
+        formKey = $.mage.cookies.get('form_key'),
+        data = {
+          action: 'add-to-wishlist',
+          form_key: formKey,
+          product: config.addToParams.data.product,
+          uenc: config.addToParams.data.uenc
+        };
+
+      $.post(url, data).done(function() {
+        const addToMessage = $.cookieStorage.get('mage-messages');
+        console.log(addToMessage);
+        // add class to mark that product is added to wishlist
+        if (addToMessage[0].type === 'success') {
+          button.addClass(config.class);
+        }
+        // delay is needed, because M2 reload customer data and msg dissapear after a second
+        setTimeout(function() {
+          customerData.set('messages', {
+            messages: addToMessage
+          });
+          // remove msg from cookie to not show after reload the page
+          $.cookieStorage.set('mage-messages', '');
+        }, 1000);
+      });
+
+    });
 
     if (items) {
       items.forEach(item => {
