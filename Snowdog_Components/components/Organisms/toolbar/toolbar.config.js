65ae7f25b385b39ac6d4384a4f676094871ad2e3
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2020-01-19 23:04:06 +0100

    #58943 - toolbars b2b fixes

--- Snowdog_Components/components/Organisms/toolbar/toolbar.config.js
+++ Snowdog_Components/components/Organisms/toolbar/toolbar.config.js
@@ -101,7 +101,37 @@ module.exports = {
         class: 'toolbar--secondary',
         showMode: false,
         showSorter: false,
-        showPager: true
+        showPager: true,
+        limiter: {
+          select: {
+            class: 'select--native toolbar__limiter margin-0',
+            label: {
+              attributes: '',
+              text: 'Show',
+              class: 'toolbar__limiter-label margin-right-s margin-bottom-0'
+            },
+            field: {
+              id: 'limiter',
+              name: 'limiter',
+              class: 'toolbar__limiter-select'
+            },
+            options: [
+              {
+                text: '12',
+                value: '12'
+              },
+              {
+                text: '24',
+                value: '24'
+              },
+              {
+                text: '36',
+                value: '36'
+              },
+            ]
+          },
+          suffix: ''
+        },
       }
     }
   ]
