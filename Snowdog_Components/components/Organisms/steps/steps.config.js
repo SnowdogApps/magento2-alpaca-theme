46b6301195125e166ba1ce105b994a78a0279439
Jarek Krupiński <jarek.krupinski@snow.dog>
2020-02-27 17:16:38 +0100

    #57250 - Update steps component

--- Snowdog_Components/components/Organisms/steps/steps.config.js
+++ Snowdog_Components/components/Organisms/steps/steps.config.js
@@ -1,29 +1,22 @@
 module.exports = {
   context: {
+    statusClass: 'steps__status--4',
     steps: [
       {
-        label: '1. Processing',
-        class: 'steps__item--completed'
+        class: 'steps__item--completed',
+        attributes: 'data-number="1" data-label="1. Processing"'
       },
       {
-        label: '2. Approved',
-        class: 'steps__item--failed'
+        class: 'steps__item--failed',
+        attributes: 'data-number="2" data-label="2. Approved"'
       },
       {
-        label: '3. Delivered',
-        class: 'steps__item--failed'
+        class: 'steps__item--failed',
+        attributes: 'data-number="3" data-label="3. Delivered"'
       },
       {
-        label: '3. Delivered',
-        class: 'steps__item--checked'
-      },
-      {
-        label: '3. Delivered',
-        class: 'steps__item--disabled'
-      },
-      {
-        label: '4. Completed',
-        class: 'steps__item--completed'
+        class: 'steps__item--completed',
+        attributes: 'data-number="4" data-label="4. Completed"'
       }
     ]
   }
