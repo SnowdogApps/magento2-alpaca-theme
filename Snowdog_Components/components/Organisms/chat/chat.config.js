b1a6ca9c5d061b3a666fd7361163fc94b1e7c16d
Jarek Krupiński <jarek.krupinski@snow.dog>
2020-03-01 15:10:56 +0100

    #57250 - Update chat component

--- Snowdog_Components/components/Organisms/chat/chat.config.js
+++ Snowdog_Components/components/Organisms/chat/chat.config.js
@@ -1,5 +1,28 @@
 module.exports = {
   context: {
-    message: 'test'
+    message: {
+      incoming: {
+        username: 'Admin',
+        text: 'Please, provide us with additional details and attach photos if possible so that we could approve your request.',
+        date: '2019-08-20 13:05:27'
+      },
+      sent: {
+        username: 'User',
+        text: 'Hello. I want to return this item because of the material, it is not as good as I expected!',
+        date: '2019-08-20 13:06:33'
+      }
+    },
+    input: {
+      label: {
+        hidden: true
+      },
+      field: {
+        class: 'chat__textarea',
+        placeholder: 'Message'
+      }
+    },
+    button: {
+      text: 'Send'
+    }
   }
 }
