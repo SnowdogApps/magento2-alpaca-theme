7ee685ba5a26223efa930af2d1b42e45731ae81a
kamil-jagoda <kamil.jagoda@gate-software.com>
2020-04-09 14:56:24 +0200

    #65714 account wishlist page styles

--- Snowdog_Components/components/Templates/dashboard/wishlist/wishlist.config.js
+++ Snowdog_Components/components/Templates/dashboard/wishlist/wishlist.config.js
@@ -33,15 +33,14 @@ module.exports = {
           name: {
             href: '#',
             class: 'wishlist__item-name-link',
-            text: 'Chaz Kangeroo Hoodie'
+            text: 'Chaz Kangeroo Hoodie product with long name more than 3 lines Chaz Kangeroo Hoodie'
           },
-          price: {
-            class: 'price-box',
-            regularPriceClass: 'price-as-configured'
+          priceSpecial: {
+            class: 'price-box'
           }
         },
         textarea: {
-          class: 'margin-0',
+          class: 'margin-0 wishlist__comment',
           field: {
             id: 'comment',
             name: 'comment',
@@ -53,18 +52,37 @@ module.exports = {
           }
         },
         qty: {
-          class: 'wishlist__qty',
-          label: {
-            text: 'Quantity:',
-            class: 'wishlist__label'
-          },
-          field: {
+          labelText: 'Quantity:',
+          input: {
             id: 'qty',
+            min: '1',
             name: 'qty',
-            type: 'number',
-            class: 'wishlist__field-qty',
-            attributes: 'value="1"'
-          }
+            defaultValue: '1',
+            ariaLabel: 'Change the quantity'
+          },
+          minusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--minus quantity-update__button--disabled',
+            attributes: 'type="button" aria-label="Decrease the quantity"',
+            icon: {
+              id: 'minus',
+              title: 'Minus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          plusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--plus',
+            attributes: 'type="button" aria-label="Increase the quantity"',
+            icon: {
+              id: 'plus',
+              title: 'Plus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          script: false
         },
         editIcon: {
           tag: 'a',
@@ -111,7 +129,7 @@ module.exports = {
           }
         },
         textarea: {
-          class: 'margin-0',
+          class: 'margin-0 wishlist__comment',
           field: {
             id: 'comment2',
             name: 'comment',
@@ -123,18 +141,304 @@ module.exports = {
           }
         },
         qty: {
-          class: 'wishlist__qty',
-          label: {
-            text: 'Quantity:',
-            class: 'wishlist__label'
-          },
-          field: {
-            id: 'qty2',
+          labelText: 'Quantity:',
+          input: {
+            id: 'qty',
+            min: '1',
             name: 'qty',
-            type: 'number',
-            class: 'wishlist__field-qty',
-            attributes: 'value="1"'
+            defaultValue: '1',
+            ariaLabel: 'Change the quantity'
+          },
+          minusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--minus quantity-update__button--disabled',
+            attributes: 'type="button" aria-label="Decrease the quantity"',
+            icon: {
+              id: 'minus',
+              title: 'Minus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          plusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--plus',
+            attributes: 'type="button" aria-label="Increase the quantity"',
+            icon: {
+              id: 'plus',
+              title: 'Plus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          script: false
+        },
+        editIcon: {
+          tag: 'a',
+          attributes: 'href="#" aria-label="Edit product parameters"',
+          class: '',
+          icon: {
+            id: 'edit',
+            title: 'Edit',
+            class: '',
+            hidden: true
           }
+        },
+        removeIcon: {
+          tag: 'button',
+          attributes: 'href="#" aria-label="Remove product from wishlist"',
+          class: '',
+          icon: {
+            id: 'remove',
+            title: 'Remove',
+            class: '',
+            hidden: true
+          }
+        },
+        addToCart: {
+          tag: 'button',
+          text: 'Add to Cart',
+          class: 'wishlist__button'
+        }
+      },
+      {
+        product: {
+          class: 'col-xs-6 col-md-4',
+          image: {
+            dataSrc: '/images/catalog-grid-item/product-color-swatch-image-1-32_40.jpg'
+          },
+          name: {
+            href: '#',
+            class: 'wishlist__item-name-link',
+            text: 'Bruno Compete Hoodie'
+          },
+          price: {
+            class: 'price-box',
+            regularPriceClass: 'price-as-configured'
+          }
+        },
+        textarea: {
+          class: 'margin-0 wishlist__comment',
+          field: {
+            id: 'comment3',
+            name: 'comment',
+            placeholder: 'Comment'
+          },
+          label: {
+            text: 'Comment',
+            hidden: true
+          }
+        },
+        qty: {
+          labelText: 'Quantity:',
+          input: {
+            id: 'qty',
+            min: '1',
+            name: 'qty',
+            defaultValue: '1',
+            ariaLabel: 'Change the quantity'
+          },
+          minusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--minus quantity-update__button--disabled',
+            attributes: 'type="button" aria-label="Decrease the quantity"',
+            icon: {
+              id: 'minus',
+              title: 'Minus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          plusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--plus',
+            attributes: 'type="button" aria-label="Increase the quantity"',
+            icon: {
+              id: 'plus',
+              title: 'Plus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          script: false
+        },
+        editIcon: {
+          tag: 'a',
+          attributes: 'href="#" aria-label="Edit product parameters"',
+          class: '',
+          icon: {
+            id: 'edit',
+            title: 'Edit',
+            class: '',
+            hidden: true
+          }
+        },
+        removeIcon: {
+          tag: 'button',
+          attributes: 'href="#" aria-label="Remove product from wishlist"',
+          class: '',
+          icon: {
+            id: 'remove',
+            title: 'Remove',
+            class: '',
+            hidden: true
+          }
+        },
+        addToCart: {
+          tag: 'button',
+          text: 'Add to Cart',
+          class: 'wishlist__button'
+        }
+      },
+      {
+        product: {
+          class: 'col-xs-6 col-md-4',
+          image: {
+            dataSrc: '/images/catalog-grid-item/product-color-swatch-image-1-32_40.jpg'
+          },
+          name: {
+            href: '#',
+            class: 'wishlist__item-name-link',
+            text: 'Bruno Compete Hoodie'
+          },
+          price: {
+            class: 'price-box',
+            regularPriceClass: 'price-as-configured'
+          }
+        },
+        textarea: {
+          class: 'margin-0 wishlist__comment',
+          field: {
+            id: 'comment4',
+            name: 'comment',
+            placeholder: 'Comment'
+          },
+          label: {
+            text: 'Comment',
+            hidden: true
+          }
+        },
+        qty: {
+          labelText: 'Quantity:',
+          input: {
+            id: 'qty',
+            min: '1',
+            name: 'qty',
+            defaultValue: '1',
+            ariaLabel: 'Change the quantity'
+          },
+          minusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--minus quantity-update__button--disabled',
+            attributes: 'type="button" aria-label="Decrease the quantity"',
+            icon: {
+              id: 'minus',
+              title: 'Minus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          plusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--plus',
+            attributes: 'type="button" aria-label="Increase the quantity"',
+            icon: {
+              id: 'plus',
+              title: 'Plus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          script: false
+        },
+        editIcon: {
+          tag: 'a',
+          attributes: 'href="#" aria-label="Edit product parameters"',
+          class: '',
+          icon: {
+            id: 'edit',
+            title: 'Edit',
+            class: '',
+            hidden: true
+          }
+        },
+        removeIcon: {
+          tag: 'button',
+          attributes: 'href="#" aria-label="Remove product from wishlist"',
+          class: '',
+          icon: {
+            id: 'remove',
+            title: 'Remove',
+            class: '',
+            hidden: true
+          }
+        },
+        addToCart: {
+          tag: 'button',
+          text: 'Add to Cart',
+          class: 'wishlist__button'
+        }
+      },
+      {
+        product: {
+          class: 'col-xs-6 col-md-4',
+          image: {
+            dataSrc: '/images/catalog-grid-item/product-color-swatch-image-1-32_40.jpg'
+          },
+          name: {
+            href: '#',
+            class: 'wishlist__item-name-link',
+            text: 'Bruno Compete Hoodie'
+          },
+          price: {
+            class: 'price-box',
+            regularPriceClass: 'price-as-configured'
+          }
+        },
+        textarea: {
+          class: 'margin-0 wishlist__comment',
+          field: {
+            id: 'comment5',
+            name: 'comment',
+            placeholder: 'Comment'
+          },
+          label: {
+            text: 'Comment',
+            hidden: true
+          }
+        },
+        qty: {
+          labelText: 'Quantity:',
+          input: {
+            id: 'qty',
+            min: '1',
+            name: 'qty',
+            defaultValue: '1',
+            ariaLabel: 'Change the quantity'
+          },
+          minusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--minus quantity-update__button--disabled',
+            attributes: 'type="button" aria-label="Decrease the quantity"',
+            icon: {
+              id: 'minus',
+              title: 'Minus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          plusQtyButton: {
+            tag: 'button',
+            class: 'quantity-update__button quantity-update__button--plus',
+            attributes: 'type="button" aria-label="Increase the quantity"',
+            icon: {
+              id: 'plus',
+              title: 'Plus mark',
+              class: 'button__icon quantity-update__icon',
+              hidden: true
+            }
+          },
+          script: true
         },
         editIcon: {
           tag: 'a',
