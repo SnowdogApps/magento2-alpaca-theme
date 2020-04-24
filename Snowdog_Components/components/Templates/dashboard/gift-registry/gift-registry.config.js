5a47dcd5b141a923a4099ea0f41905b287b8709b
kamil-jagoda <kamil.jagoda@gate-software.com>
2020-04-24 11:05:04 +0200

    #65717 gift registry - share component + theme

--- Snowdog_Components/components/Templates/dashboard/gift-registry/gift-registry.config.js
+++ Snowdog_Components/components/Templates/dashboard/gift-registry/gift-registry.config.js
@@ -124,5 +124,118 @@ module.exports = {
         ]
       }
     }
-  }
+  },
+  variants: [
+    {
+      name: 'share',
+      title: 'Share Gift Registry',
+      context: {
+        heading: {
+          tag: 'h1',
+          class: 'heading dashboard__content-heading',
+          text: 'Share Gift Registry'
+        },
+        headingForm: {
+          class: 'dashboard-form__title',
+          tag: 'h2',
+          text: 'Share "Gift Name" Gift Registry'
+        },
+        fieldset: {
+          class: 'margin-vc-m'
+        },
+        legendSender: {
+          class: '',
+          text: 'Sender'
+        },
+        legendInvite: {
+          class: '',
+          text: 'Invitee'
+        },
+        nameInput: {
+          class: 'required',
+          label: {
+            text: 'Name',
+          },
+          field: {
+            id: 'name',
+            name: 'name',
+            placeholder: '',
+            type: 'text'
+          }
+        },
+        firstNameInput: {
+          class: 'required',
+          label: {
+            text: 'First Name',
+          },
+          field: {
+            id: 'first-name',
+            name: 'first-name',
+            placeholder: '',
+            type: 'text'
+          }
+        },
+        emailInput: {
+          class: 'required',
+          label: {
+            text: 'Email',
+          },
+          field: {
+            id: 'email_1',
+            name: 'email_1',
+            placeholder: '',
+            type: 'text'
+          }
+        },
+        textarea: {
+          label: {
+            text: 'Message',
+          },
+          field: {
+            id: 'message',
+            name: 'message',
+            placeholder: '',
+            type: 'text'
+          }
+        },
+        buttonRemove: {
+          tag: 'button',
+          attributes: 'type="button" aria-label="button"',
+          class: 'button--secondary margin-bottom-m remove',
+          text: 'Remove'
+        },
+        buttonAdd: {
+          class: 'margin-vc-m',
+          tag: 'button',
+          attributes: 'type="button" aria-label="button"',
+          text: 'Add Invitee'
+        },
+        actions: {
+          sides: [
+            {
+              action: [
+                {
+                  button: {
+                    text: 'Share Gift Registry',
+                    class: 'actions-group__button'
+                  }
+                }
+              ]
+            },
+            {
+              action: [
+                {
+                  link: {
+                    text: 'Back',
+                    title: 'Back',
+                    class: 'actions-group__link'
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      }
+    },
+  ]
 }
