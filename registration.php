11882fa38c630f77b9db09d634b34315873d5f4d
Ania <anna.karon@snow.dog>
2020-05-05 14:47:42 +0200

    Revert "66233 theme & composer name change to alpaca"
    This reverts commit 3093fa6ca5fd16dc648b434125887cd70bfd4389.

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Snowdog/alpaca',
+    'frontend/Snowdog/colibri',
     __DIR__
 );
