0c302529aba3b7f9486592e36cb6c7038ae41828
kamil-jagoda <kamil.jagoda@gate-software.com>
2020-05-06 14:02:01 +0200

    #65998 init prompt edit wishlist mixin method

---
+++ Magento_MultipleWishlist/web/js/prompt-edit-wishlist-mixin.js
@@ -0,0 +1,28 @@
+define(['jquery'], function ($) {
+    'use strict';
+
+    return function (widget) {
+        $.widget('mage.promptEditWithList', widget, {
+            /**
+             * @return {*|jQuery|HTMLElement}
+             */
+            getNoticeTemplate: function () {
+                var template = $('<div class="field choice">' +
+                    '<input id="wishlist-public" type="checkbox" name="' + this.options.isPublicFieldAttributeName + '">' +
+                    '<label for="wishlist-public" class="label">' +
+                    '<span>' + this.options.isPublicFieldLabel + '</span>' +
+                    '</label>' +
+                    '<input name="form_key" type="hidden" value="' + this.options.hiddenFieldValue + '">' +
+                    '</div>');
+
+                if (this.options.isPublicFieldChecked) {
+                    template.find('input[type = checkbox]').attr('checked', 'checked');
+                }
+
+                return template;
+            }
+        });
+
+        return $.mage.promptEditWithList;
+    };
+});
