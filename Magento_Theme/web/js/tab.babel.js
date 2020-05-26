044ad5eeb0146f24706a00889310342ff1c5bb8d
Ania <anna.karon@snow.dog>
2020-05-26 12:17:20 +0200

    fix tab script, merge issue

--- Magento_Theme/web/js/tab.babel.js
+++ Magento_Theme/web/js/tab.babel.js
@@ -7,30 +7,8 @@ define([], function () {
       tabItems = Array.from(element.children),
       stickyLabels = document.querySelectorAll('.tab__title--sticky'),
       allItems = [...tabItems, ...stickyLabels];
-  function clean(item) {
-    if (item.classList.contains('tab__content')) {
-      item.classList.remove(activeContentClass);
-      item.setAttribute('aria-hidden', true);
-    }
-    else {
-      item.classList.remove(activeTitleClass);
-      item.setAttribute('aria-expanded', false);
-      item.setAttribute('aria-selected', false);
-    }
-  }
 
-  function setActiveContent(item, tabTitle) {
-    if (item.classList.contains('tab__content') && tabTitle === item.dataset.content) {
-      item.classList.add(activeContentClass);
-      item.setAttribute('aria-hidden', false);
-    }
-    else if (tabTitle === item.dataset.tab) {
-      item.classList.add(activeTitleClass);
-      item.setAttribute('aria-expanded', true);
-      item.setAttribute('aria-selected', true);
-    }
-
-    allItems.forEach(item => {
+    allItems.forEach((item) => {
       const tabTitle = item.dataset.tab;
 
       if (item.classList.contains('tab__title')) {
@@ -42,5 +20,29 @@ define([], function () {
         });
       }
     });
+
+    function clean(item) {
+      if (item.classList.contains('tab__content')) {
+        item.classList.remove(activeContentClass);
+        item.setAttribute('aria-hidden', true);
+      }
+      else {
+        item.classList.remove(activeTitleClass);
+        item.setAttribute('aria-expanded', false);
+        item.setAttribute('aria-selected', false);
+      }
+    }
+
+    function setActiveContent(item, tabTitle) {
+      if (item.classList.contains('tab__content') && tabTitle === item.dataset.content) {
+        item.classList.add(activeContentClass);
+        item.setAttribute('aria-hidden', false);
+      }
+      else if (tabTitle === item.dataset.tab) {
+        item.classList.add(activeTitleClass);
+        item.setAttribute('aria-expanded', true);
+        item.setAttribute('aria-selected', true);
+      }
+    }
   };
 });
