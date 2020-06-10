b9b256731e0cdbc6b6531e470a8e9dd4f3e73c5e
Ania <anna.karon@snow.dog>
2020-06-10 11:51:56 +0200

    67863 update bundle PDP

--- Magento_Bundle/requirejs-config.js
+++ Magento_Bundle/requirejs-config.js
@@ -2,8 +2,11 @@ var config = {
   config: {
     mixins: {
       "Magento_Bundle/js/price-bundle": {
-        "Magento_Bundle/js/mixins/price-bundle": true
-      }
-    }
-  }
+        "Magento_Bundle/js/mixins/price-bundle": true,
+      },
+      "Magento_Bundle/js/product-summary": {
+        "Magento_Bundle/js/mixins/product-summary": true,
+      },
+    },
+  },
 };
