851037264abc47df36d9330eadbc251e7901f91c
Jarek <jarek.krupinski@snow.dog>
2020-09-14 13:13:28 +0200

    #70656 - Remove options, close modal on cancel

--- Magento_Msrp/web/js/msrp-mixin.js
+++ Magento_Msrp/web/js/msrp-mixin.js
@@ -2,65 +2,49 @@ define(['jquery'], function ($) {
   'use strict';
   var widgetMixin = {
     options: {
-      showAddToCart: true,
-      submitUrl: '',
-      cartButtonId: '',
-      singleOpenDropDown: true,
-      dialog: {}, // Options for mage/dropdown
-      dialogDelay: 500, // Delay in ms after resize dropdown shown again
-      origin: '', //Required, type of popup: 'msrp', 'tier' or 'info' popup
-
-      // Selectors
-      cartForm: '.form.map.checkout',
-      msrpLabelId: '#map-popup-msrp',
-      msrpPriceElement: '#map-popup-msrp .price-wrapper',
-      priceLabelId: '#map-popup-price',
-      priceElement: '#map-popup-price .price',
-      mapInfoLinks: '.map-show-info',
-      displayPriceElement: '.old-price.map-old-price .price-wrapper',
-      fallbackPriceElement: '.normal-price.map-fallback-price .price-wrapper',
-      displayPriceContainer: '.old-price.map-old-price',
-      fallbackPriceContainer: '.normal-price.map-fallback-price',
-      popUpAttr: '[data-role=msrp-popup-template]',
-      popupCartButtonId: '#map-popup-button',
-      paypalCheckoutButons: '[data-action=checkout-form-submit]',
-      popupId: '',
-      realPrice: '',
-      isSaleable: '',
-      msrpPrice: '',
-      helpLinkId: '',
-      addToCartButton: '',
-
-      // Text options
-      productName: '',
-      addToCartUrl: ''
+      cancelButton: '.msrp__cancel-button',
     },
-
-    openDropDown: null,
-    triggerClass: 'dropdown-active',
 
     popUpOptions: {
-        appendTo: 'body',
-        dialogContentClass: 'modal__container',
-        closeOnMouseLeave: false,
-        autoPosition: false,
-        closeOnClickOutside: false,
-        dialogClass: 'modal modal--active',
-        position: {
-            my: 'left top',
-            collision: 'fit none',
-            at: 'left bottom',
-            within: 'body'
-        },
-        shadowHinter: 'popup popup-pointer'
+      dialogContentClass: 'modal__container msrp__modal-container',
+      autoPosition: false,
+      dialogClass: 'modal modal--active msrp__modal',
     },
-    popupOpened: false,
-    wasOpened: false
+
+    initMsrpPopup: function () {
+      var popupDOM = $(this.options.popUpAttr)[0],
+        $msrpPopup = $(popupDOM.innerHTML.trim()),
+        self = this;
+
+      $msrpPopup.find(this.options.productIdInput).val(this.options.productId);
+      $('body').append($msrpPopup);
+      $msrpPopup.trigger('contentUpdated');
+
+      $msrpPopup
+        .find('.msrp__tocart-button')
+        .on('click', this.handleMsrpAddToCart.bind(this))
+        .filter(this.options.popupCartButtonId)
+        .text($(this.options.addToCartButton).text());
+
+      $msrpPopup
+        .find(this.options.paypalCheckoutButons)
+        .on('click', this.handleMsrpPaypalCheckout.bind(this));
+
+      $(this.options.popupId).on('click', this.openPopup.bind(this));
+
+      this.$popup = $msrpPopup;
+
+      if (this.options.cancelButton) {
+        this.$popup.find(this.options.cancelButton).on('click', function () {
+          self.closePopup(self.$popup);
+        });
+      }
+    }
   };
 
   return function (targetWidget) {
-      $.widget('mage.addToCart', targetWidget, widgetMixin);
+    $.widget('mage.addToCart', targetWidget, widgetMixin);
 
-      return $.mage.addToCart;
+    return $.mage.addToCart;
   };
 });
