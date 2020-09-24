0c750f17c8fe378848acef200b2da109444ebb80
Ola <ola.frankowska@snow.dog>
2020-09-24 13:36:54 +0200

    #70550 create downloadable mixin, copy functions

---
+++ Magento_Downloadable/web/js/downloadable-mixin.js
@@ -0,0 +1,59 @@
+define([
+    'jquery',
+    'jquery-ui-modules/widget',
+    'Magento_Catalog/js/price-box'
+], function ($) {
+    'use strict';
+
+  return function (widget) {
+    /**
+     * Downloadable widget
+     */
+    $.widget('mage.downloadable', widget, {
+        _create: function () {
+          var self = this;
+
+          this.element.find(this.options.linkElement).on('change', $.proxy(function () {
+              this._reloadPrice();
+          }, this));
+
+          this.element.find(this.options.allElements).on('change', function () {
+              if (this.checked) {
+                  $('label[for="' + this.id + '"] > span').text($(this).attr('data-checked'));
+                  self.element.find(self.options.linkElement + ':not(:checked)').each(function () {
+                      $(this).trigger('click');
+                  });
+              } else {
+                  $('[for="' + this.id + '"] > span').text($(this).attr('data-notchecked'));
+                  self.element.find(self.options.linkElement + ':checked').each(function () {
+                      $(this).trigger('click');
+                  });
+              }
+          });
+
+          this._reloadPrice();
+        },
+        reloadAllCheckText: function () {
+            var allChecked = true,
+                allElementsCheck = $(this.options.allElements),
+                allElementsLabel = $('label[for="' + allElementsCheck.attr('id') + '"] > span');
+
+            $(this.options.linkElement).each(function () {
+                if (!this.checked) {
+                    allChecked = false;
+                }
+            });
+
+            if (allChecked) {
+                allElementsLabel.text(allElementsCheck.attr('data-checked'));
+                allElementsCheck.prop('checked', true);
+            } else {
+                allElementsLabel.text(allElementsCheck.attr('data-notchecked'));
+                allElementsCheck.prop('checked', false);
+            }
+        }
+    });
+
+    return $.mage.downloadable;
+  }
+});
