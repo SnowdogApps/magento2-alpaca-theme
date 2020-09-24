fd580aa383cebb8b6be0ed67694a09ac23a70d51
Ola <ola.frankowska@snow.dog>
2020-09-24 13:37:51 +0200

    #70550 adjust downloadable js functions

--- Magento_Downloadable/web/js/downloadable-mixin.js
+++ Magento_Downloadable/web/js/downloadable-mixin.js
@@ -11,34 +11,34 @@ define([
      */
     $.widget('mage.downloadable', widget, {
         _create: function () {
-          var self = this;
+            var self = this;
 
-          this.element.find(this.options.linkElement).on('change', $.proxy(function () {
-              this._reloadPrice();
-          }, this));
+            this.element.find(this.options.linkElement).on('change', $.proxy(function () {
+                this._reloadPrice();
+            }, this));
 
-          this.element.find(this.options.allElements).on('change', function () {
-              if (this.checked) {
-                  $('label[for="' + this.id + '"] > span').text($(this).attr('data-checked'));
-                  self.element.find(self.options.linkElement + ':not(:checked)').each(function () {
-                      $(this).trigger('click');
-                  });
-              } else {
-                  $('[for="' + this.id + '"] > span').text($(this).attr('data-notchecked'));
-                  self.element.find(self.options.linkElement + ':checked').each(function () {
-                      $(this).trigger('click');
-                  });
-              }
-          });
+            this.element.find(this.options.allElements).on('change', function () {
+                if (this.checked) {
+                    $('label[for="' + this.id + '"]').text($(this).attr('data-checked'));
+                    self.element.find(self.options.linkElement + ':not(:checked)').each(function () {
+                        $(this).trigger('click');
+                    });
+                } else {
+                    $('[for="' + this.id + '"]').text($(this).attr('data-notchecked'));
+                    self.element.find(self.options.linkElement + ':checked').each(function () {
+                        $(this).trigger('click');
+                    });
+                }
+            });
 
-          this._reloadPrice();
+            this._reloadPrice();
         },
         reloadAllCheckText: function () {
             var allChecked = true,
                 allElementsCheck = $(this.options.allElements),
-                allElementsLabel = $('label[for="' + allElementsCheck.attr('id') + '"] > span');
+                allElementsLabel = $('label[for="' + allElementsCheck.attr('id') + '"]');
 
-            $(this.options.linkElement).each(function () {
+            this.element.find(this.options.linkElement).each(function () {
                 if (!this.checked) {
                     allChecked = false;
                 }
