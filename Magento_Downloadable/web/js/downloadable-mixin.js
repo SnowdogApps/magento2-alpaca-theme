ec95ce803b0d8b77bfff984b127abccf7625e943
Ola <ola.frankowska@snow.dog>
2020-09-24 15:44:35 +0200

    #70550 fix indentation

--- Magento_Downloadable/web/js/downloadable-mixin.js
+++ Magento_Downloadable/web/js/downloadable-mixin.js
@@ -10,48 +10,48 @@ define([
      * Downloadable widget
      */
     $.widget('mage.downloadable', widget, {
-        _create: function () {
-            var self = this;
+      _create: function () {
+        var self = this;
 
-            this.element.find(this.options.linkElement).on('change', $.proxy(function () {
-                this._reloadPrice();
-            }, this));
+        this.element.find(this.options.linkElement).on('change', $.proxy(function () {
+          this._reloadPrice();
+        }, this));
 
-            this.element.find(this.options.allElements).on('change', function () {
-                if (this.checked) {
-                    $('label[for="' + this.id + '"]').text($(this).attr('data-checked'));
-                    self.element.find(self.options.linkElement + ':not(:checked)').each(function () {
-                        $(this).trigger('click');
-                    });
-                } else {
-                    $('[for="' + this.id + '"]').text($(this).attr('data-notchecked'));
-                    self.element.find(self.options.linkElement + ':checked').each(function () {
-                        $(this).trigger('click');
-                    });
-                }
+        this.element.find(this.options.allElements).on('change', function () {
+          if (this.checked) {
+            $('label[for="' + this.id + '"]').text($(this).attr('data-checked'));
+            self.element.find(self.options.linkElement + ':not(:checked)').each(function () {
+              $(this).trigger('click');
             });
-
-            this._reloadPrice();
-        },
-        reloadAllCheckText: function () {
-            var allChecked = true,
-                allElementsCheck = $(this.options.allElements),
-                allElementsLabel = $('label[for="' + allElementsCheck.attr('id') + '"]');
-
-            this.element.find(this.options.linkElement).each(function () {
-                if (!this.checked) {
-                    allChecked = false;
-                }
+          } else {
+            $('[for="' + this.id + '"]').text($(this).attr('data-notchecked'));
+            self.element.find(self.options.linkElement + ':checked').each(function () {
+              $(this).trigger('click');
             });
+          }
+        });
 
-            if (allChecked) {
-                allElementsLabel.text(allElementsCheck.attr('data-checked'));
-                allElementsCheck.prop('checked', true);
-            } else {
-                allElementsLabel.text(allElementsCheck.attr('data-notchecked'));
-                allElementsCheck.prop('checked', false);
-            }
+        this._reloadPrice();
+      },
+      reloadAllCheckText: function () {
+        var allChecked = true,
+          allElementsCheck = $(this.options.allElements),
+          allElementsLabel = $('label[for="' + allElementsCheck.attr('id') + '"]');
+
+        this.element.find(this.options.linkElement).each(function () {
+          if (!this.checked) {
+            allChecked = false;
+          }
+        });
+
+        if (allChecked) {
+          allElementsLabel.text(allElementsCheck.attr('data-checked'));
+          allElementsCheck.prop('checked', true);
+        } else {
+          allElementsLabel.text(allElementsCheck.attr('data-notchecked'));
+          allElementsCheck.prop('checked', false);
         }
+      }
     });
 
     return $.mage.downloadable;
