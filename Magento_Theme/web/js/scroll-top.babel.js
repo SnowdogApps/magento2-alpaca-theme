36b6b47dfb1db7e7ea4530578a4d94ea8f378c70
Ania <anna.karon@snow.dog>
2020-10-06 11:25:46 +0200

    66038 update focus management in scroll top script

--- Magento_Theme/web/js/scroll-top.babel.js
+++ Magento_Theme/web/js/scroll-top.babel.js
@@ -3,7 +3,8 @@ define([], function () {
 
   return function (options, element) {
     element.addEventListener('click', function () {
-      const firstFocusable = document.getElementById('skip-nav-top');
+      const focusable = document.querySelectorAll('a[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), object, embed, *[tabindex]:not([tabindex="-1"])');
+      const firstFocusable = focusable[0];
       window.scrollTo({
         top: 0,
         left: 0,
