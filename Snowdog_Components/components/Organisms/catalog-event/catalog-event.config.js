58b373270841503bde90bc732a5b90bf8046cfc3
Ola <ola.frankowska@snow.dog>
2020-11-02 15:04:46 +0100

    #72233 add catalog event component, adjust template

---
+++ Snowdog_Components/components/Organisms/catalog-event/catalog-event.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  context: {
+    class: 'section',
+    heading: {
+      tag: 'h2',
+      class: 'heading heading--second-level text-center margin-bottom-0',
+      text: 'Sale ends in',
+    },
+    ticker: {
+      class: 'list',
+      elements: [
+        {
+          class: 'list__item',
+          value: {
+            class: '',
+            text: '5'
+          },
+          label: {
+            class: '',
+            text: 'Dayclears'
+          }
+        },
+        {
+          class: 'list__item',
+          value: {
+            class: '',
+            text: '23'
+          },
+          label: {
+            class: '',
+            text: 'Hrs'
+          }
+        },
+        {
+          class: 'list__item',
+          value: {
+            class: '',
+            text: '45'
+          },
+          label: {
+            class: '',
+            text: 'Mins'
+          }
+        }
+      ]
+    }
+  }
+}
