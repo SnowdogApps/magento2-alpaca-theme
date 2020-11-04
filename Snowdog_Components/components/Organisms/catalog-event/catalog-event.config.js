545e9c5e6b211e4e3c83d82c0fa54be4ec241654
Ola <ola.frankowska@snow.dog>
2020-11-04 12:21:56 +0100

    #72233 add catalog event slider variant

--- Snowdog_Components/components/Organisms/catalog-event/catalog-event.config.js
+++ Snowdog_Components/components/Organisms/catalog-event/catalog-event.config.js
@@ -3,46 +3,108 @@ module.exports = {
     class: 'section',
     heading: {
       tag: 'h2',
-      class: 'heading heading--second-level text-center margin-bottom-0',
+      class: 'heading heading--second-level margin-bottom-0',
       text: 'Sale ends in',
     },
-    ticker: {
+    date: {
       class: 'list',
       elements: [
         {
           class: 'list__item',
           value: {
-            class: '',
+            class: 'value',
             text: '5'
           },
           label: {
-            class: '',
+            class: 'label',
             text: 'Dayclears'
           }
         },
         {
           class: 'list__item',
           value: {
-            class: '',
+            class: 'value',
             text: '23'
           },
           label: {
-            class: '',
+            class: 'label',
             text: 'Hrs'
           }
         },
         {
           class: 'list__item',
           value: {
-            class: '',
+            class: 'value',
             text: '45'
           },
           label: {
-            class: '',
+            class: 'label',
             text: 'Mins'
           }
         }
       ]
     }
-  }
+  },
+  variants: [
+    {
+      name: 'slider',
+      context: {
+        class: 'catalog-event--slider',
+        heading: {
+          tag: 'h2',
+          class: 'heading heading--second-level',
+          text: 'Hoodies & Sweatshirts',
+        },
+        subheading: {
+          tag: 'h3',
+          class: 'heading heading--third-level margin-bottom-0',
+          text: 'Sale ends in',
+        },
+        date: {
+          class: 'list',
+          elements: [
+            {
+              class: 'list__item',
+              value: {
+                class: 'value',
+                text: '5'
+              },
+              label: {
+                class: 'label',
+                text: 'Dayclears'
+              }
+            },
+            {
+              class: 'list__item',
+              value: {
+                class: 'value',
+                text: '23'
+              },
+              label: {
+                class: 'label',
+                text: 'Hrs'
+              }
+            },
+            {
+              class: 'list__item',
+              value: {
+                class: 'value',
+                text: '45'
+              },
+              label: {
+                class: 'label',
+                text: 'Mins'
+              }
+            }
+          ]
+        },
+        button: {
+          tag: 'a',
+          class: 'catalog-event__button',
+          attributes: 'href="#"',
+          text: 'Go to sale'
+        }
+      }
+    }
+  ]
 }
