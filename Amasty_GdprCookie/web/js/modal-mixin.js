35d701d4c56c06916ceb463705ee4c0a73a45d44
Jarek <jarek.krupinski@snow.dog>
2021-05-06 11:06:56 +0200

    #77501 - Formatting

--- Amasty_GdprCookie/web/js/modal-mixin.js
+++ Amasty_GdprCookie/web/js/modal-mixin.js
@@ -1,49 +1,51 @@
 define([
-    'uiComponent',
-    'jquery',
-    'underscore',
-    'Magento_Ui/js/modal/modal'
+  "uiComponent",
+  "jquery",
+  "underscore",
+  "Magento_Ui/js/modal/modal",
 ], function (Component, $, _, modal) {
-    'use strict';
+  "use strict";
 
-    return function (Component) {
-        return Component.extend({
-            defaults: {
-                template: {
-                    afterRender: function () {
-                    }
-                }
-            },
+  return function (Component) {
+    return Component.extend({
+      defaults: {
+        template: {
+          afterRender: function () {},
+        },
+      },
 
-            initModal: function () {
-                var options = {
-                    type: 'popup',
-                    responsive: true,
-                    modalClass: '',
-                    buttons: []
-                };
+      initModal: function () {
+        var options = {
+          type: "popup",
+          responsive: true,
+          modalClass: "",
+          buttons: [],
+        };
 
-                this.isShowModal(true);
+        this.isShowModal(true);
 
-                if (this.websiteInteraction == 1) {
-                    options.clickableOverlay = false;
-                    options.keyEventHandlers = {
-                        escapeKey: function () { }
-                    };
+        if (this.websiteInteraction == 1) {
+          options.clickableOverlay = false;
+          options.keyEventHandlers = {
+            escapeKey: function () {},
+          };
 
-                    options.opened = function () {
-                        $('.modal-header button.action-close').hide();
-                    };
-                }
+          options.opened = function () {
+            $(".modal-header button.action-close").hide();
+          };
+        }
 
-                this.cookieModal = modal(options, this.element.modal);
+        this.cookieModal = modal(options, this.element.modal);
 
-                this.cookieModal.element.html($(this.element.container));
-                this.cookieModal.openModal().on('modalclosed', function () {
-                    this.cookieModal.element.html('');
-                    $(window).off('resize', this.resizeFunc);
-                }.bind(this));
-            },
-        });
-    };
+        this.cookieModal.element.html($(this.element.container));
+        this.cookieModal.openModal().on(
+          "modalclosed",
+          function () {
+            this.cookieModal.element.html("");
+            $(window).off("resize", this.resizeFunc);
+          }.bind(this)
+        );
+      },
+    });
+  };
 });
