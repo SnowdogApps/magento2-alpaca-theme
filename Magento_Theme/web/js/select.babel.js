c7928a742aa673ceddbff975fde1e247dda3a5f4
rjwaberski <radek.waberski@snow.dog>
2021-08-04 10:57:36 +0200

    #80105 select search component

--- Magento_Theme/web/js/select.babel.js
+++ Magento_Theme/web/js/select.babel.js
@@ -6,17 +6,46 @@ define([
   'use strict'
 
   return function (config, select) {
+    const isSearchable = $(select).hasClass('select--search')
+
+    function matchCustom(params, data) {
+      if ($(data.element).data('option') === 'fallback') {
+        return data;
+      }
+
+      if ($.trim(params.term) === '') {
+        return data;
+      }
+
+      if (typeof data.text === 'undefined') {
+        return null;
+      }
+
+      if (data.text.toLowerCase().indexOf(params.term.toLowerCase()) > -1) {
+        return data;
+      }
+
+      return null;
+    }
+
     const selectId = select.id,
       defaultOptions = {
-        minimumResultsForSearch: Infinity,
+        minimumResultsForSearch: isSearchable ? undefined : Infinity,
         width: null,
         position: 'bottom',
         debug: true
       }
+
     if (config.type === 'number') {
       defaultOptions.sorter = (data) =>
         data.sort((a, b) => a.text.localeCompare(b.text))
     }
+
+    if (isSearchable) {
+      defaultOptions.dropdownCssClass = 'select-dropdown--search'
+      defaultOptions.matcher = matchCustom
+    }
+
     config.options = Object.assign(defaultOptions, config.options)
     $(`#${selectId}`).select2(config.options)
   }
