005815af701e1246336153aab0b81a75b75f88d6
Ola <ola.frankowska@snow.dog>
2021-08-06 18:04:02 +0200

    #72752 add passive listeners

--- web/jquery.js
+++ web/jquery.js
@@ -11010,5 +11010,26 @@
 		window.jQuery = window.$ = jQuery;
 	}
 
+	jQuery.event.special.touchstart = {
+	    setup: function( _, ns, handle ) {
+	        this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+	    }
+	};
+	jQuery.event.special.touchmove = {
+	    setup: function( _, ns, handle ) {
+	        this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+	    }
+	};
+	jQuery.event.special.wheel = {
+	    setup: function( _, ns, handle ){
+	        this.addEventListener("wheel", handle, { passive: true });
+	    }
+	};
+	jQuery.event.special.mousewheel = {
+	    setup: function( _, ns, handle ){
+	        this.addEventListener("mousewheel", handle, { passive: true });
+	    }
+	};
+
 	return jQuery;
 }));
