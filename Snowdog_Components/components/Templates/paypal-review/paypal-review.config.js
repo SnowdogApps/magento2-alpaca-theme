0df3f27c9c63d3ca661136157eddc8b0cbb2d239
Michał Romaszkin <michal.romaszkin@snow.dog>
2021-08-11 18:56:11 +0200

    #81624: Fix quotes, update mobile styles and scss variables

--- Snowdog_Components/components/Templates/paypal-review/paypal-review.config.js
+++ Snowdog_Components/components/Templates/paypal-review/paypal-review.config.js
@@ -1,34 +1,34 @@
 module.exports = {
   context: {
     heading: {
-      tag: "h1",
-      class: "heading--page",
-      text: "Review order",
+      tag: 'h1',
+      class: 'heading--page',
+      text: 'Review order',
     },
     shippingMethodSelect: {
-      class: "select--native",
+      class: 'select--native',
       label: {
-        attributes: "",
-        text: "Shipping Method",
+        attributes: '',
+        text: 'Shipping Method',
       },
     },
     shippingAddress: {
       title: {
-        tag: "h2",
-        class: "heading--third-level",
-        text: "Shipping Address",
+        tag: 'h2',
+        class: 'heading--third-level',
+        text: 'Shipping Address',
       },
       details:
         'John Doe<br> ul. Mostowa 11<br> Poznań, Armed Forces Africa 60-688<br> United States<br> <a href="tel:887887887"> 887887887 </a>',
       actions: {
-        class: "actions-toolbar",
+        class: 'actions-toolbar',
         sides: [
           {
             action: [
               {
                 link: {
-                  text: "Edit",
-                  class: "actions-group__link padding-top-xs",
+                  text: 'Edit',
+                  class: 'actions-group__link padding-top-xs',
                 },
               },
             ],
@@ -38,20 +38,20 @@ module.exports = {
     },
     paymentMethod: {
       title: {
-        tag: "h2",
-        class: "heading--third-level",
-        text: "Payment method",
+        tag: 'h2',
+        class: 'heading--third-level',
+        text: 'Payment method',
       },
-      details: "PayPal<br> qwe@qwe.com",
+      details: 'PayPal<br> qwe@qwe.com',
       actions: {
-        class: "actions-toolbar",
+        class: 'actions-toolbar',
         sides: [
           {
             action: [
               {
                 link: {
-                  text: "Edit Payment Information",
-                  class: "actions-group__link padding-top-xs",
+                  text: 'Edit Payment Information',
+                  class: 'actions-group__link padding-top-xs',
                 },
               },
             ],
@@ -61,19 +61,19 @@ module.exports = {
     },
     shoppingCart: {
       title: {
-        tag: "h2",
-        class: "heading--third-level",
-        text: "Items in your shopping cart",
+        tag: 'h2',
+        class: 'heading--third-level',
+        text: 'Items in your shopping cart',
       },
       actions: {
-        class: "actions-toolbar",
+        class: 'actions-toolbar',
         sides: [
           {
             action: [
               {
                 link: {
-                  text: "Edit Shopping Cart",
-                  class: "actions-group__link padding-top-0",
+                  text: 'Edit Shopping Cart',
+                  class: 'actions-group__link padding-top-0',
                 },
               },
             ],
@@ -82,33 +82,33 @@ module.exports = {
       },
     },
     table: {
-      class: "table--tfoot-scope-row",
+      class: 'table--tfoot-scope-row',
       mainTags: [
         {
-          mainTag: "thead",
+          mainTag: 'thead',
           rowTags: [
             {
-              rowTag: "tr",
+              rowTag: 'tr',
               rowTagAttributes: 'role="row"',
               childTags: [
                 {
-                  childTag: "th",
-                  content: "Item",
+                  childTag: 'th',
+                  content: 'Item',
                   childTagAttributes: 'scope="col" role="columnheader"',
                 },
                 {
-                  childTag: "th",
-                  content: "Price",
+                  childTag: 'th',
+                  content: 'Price',
                   childTagAttributes: 'scope="col" role="columnheader"',
                 },
                 {
-                  childTag: "th",
-                  content: "Qty",
+                  childTag: 'th',
+                  content: 'Qty',
                   childTagAttributes: 'scope="col" role="columnheader"',
                 },
                 {
-                  childTag: "th",
-                  content: "Subtotal",
+                  childTag: 'th',
+                  content: 'Subtotal',
                   childTagAttributes: 'scope="col" role="columnheader"',
                 },
               ],
@@ -116,160 +116,160 @@ module.exports = {
           ],
         },
         {
-          mainTag: "tbody",
+          mainTag: 'tbody',
           rowTags: [
             {
-              rowTag: "tr",
+              rowTag: 'tr',
               rowTagAttributes: 'role="row"',
               childTags: [
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Item:" role="gridcell"',
                   content:
                     '<div class="row middle-sm"><img class="paypal-review__product-image lazyload margin-right-m" src="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAABCAQAAABN/Pf1AAAAC0lEQVR42mNkwAIAACoAAgu1Hc4AAAAASUVORK5CYII=" data-src="/images/product/product-160_240.jpg" alt="Image of product Bluzka \'ONLSANSA SINGLET WVN\'"><div class="paypal-review__product-details"><span class="paypal-review__product-name">Bluzka \'ONLSANSA SINGLET WVN\'</span></div></div>',
                 },
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Price:" role="gridcell"',
-                  content: "$159.95",
+                  content: '$159.95',
                 },
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Qty:" role="gridcell"',
-                  content: "1",
+                  content: '1',
                 },
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Subtotal:" role="gridcell"',
-                  content: "$159.95",
+                  content: '$159.95',
                 },
               ],
             },
             {
-              rowTag: "tr",
+              rowTag: 'tr',
               rowTagAttributes: 'role="row"',
               childTags: [
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Item:" role="gridcell"',
                   content:
                     '<div class="row middle-sm"><img class="paypal-review__product-image lazyload margin-right-m" src="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAABCAQAAABN/Pf1AAAAC0lEQVR42mNkwAIAACoAAgu1Hc4AAAAASUVORK5CYII=" data-src="/images/product/product-160_240.jpg" alt="Image of product Bluzka \'ONLSANSA SINGLET WVN\'"><span class="paypal-review__product-name">Bluzka \'ONLSANSA SINGLET WVN\'</span></div>',
                 },
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Price:" role="gridcell"',
-                  content: "$159.95",
+                  content: '$159.95',
                 },
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Qty:" role="gridcell"',
-                  content: "1",
+                  content: '1',
                 },
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Subtotal:" role="gridcell"',
-                  content: "$159.95",
+                  content: '$159.95',
                 },
               ],
             },
             {
-              rowTag: "tr",
+              rowTag: 'tr',
               rowTagAttributes: 'role="row"',
               childTags: [
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Item:" role="gridcell"',
                   content:
                     '<div class="row middle-sm"><img class="paypal-review__product-image lazyload margin-right-m" src="data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAABCAQAAABN/Pf1AAAAC0lEQVR42mNkwAIAACoAAgu1Hc4AAAAASUVORK5CYII=" data-src="/images/product/product-160_240.jpg" alt="Image of product Bluzka \'ONLSANSA SINGLET WVN\'"><span class="paypal-review__product-name">Bluzka \'ONLSANSA SINGLET WVN\'</span></div>',
                 },
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Price:" role="gridcell"',
-                  content: "$159.95",
+                  content: '$159.95',
                 },
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Qty:" role="gridcell"',
-                  content: "1",
+                  content: '1',
                 },
                 {
-                  childTag: "td",
+                  childTag: 'td',
                   childTagAttributes: 'data-th="Subtotal:" role="gridcell"',
-                  content: "$159.95",
+                  content: '$159.95',
                 },
               ],
             },
           ],
         },
         {
-          mainTag: "tfoot",
+          mainTag: 'tfoot',
           rowTags: [
             {
-              rowTag: "tr",
+              rowTag: 'tr',
               rowTagAttributes: 'role="row"',
               childTags: [
                 {
-                  childTag: "th",
-                  content: "Subtotal",
+                  childTag: 'th',
+                  content: 'Subtotal',
                   childTagAttributes:
                     'colspan=\'3\' role="rowheader" scope="row"',
                 },
                 {
-                  childTag: "td",
-                  content: "$480",
+                  childTag: 'td',
+                  content: '$480',
                   childTagAttributes: 'data-th="Subtotal:" role="gridcell"',
                 },
               ],
             },
             {
-              rowTag: "tr",
+              rowTag: 'tr',
               rowTagAttributes: 'role="row"',
               childTags: [
                 {
-                  childTag: "th",
+                  childTag: 'th',
                   content:
-                    "Shipping & Handling (Expedited Shipping - UPS Worldwide Expedited)",
+                    'Shipping & Handling (Expedited Shipping - UPS Worldwide Expedited)',
                   childTagAttributes:
                     'colspan=\'3\' role="rowheader" scope="row"',
                 },
                 {
-                  childTag: "td",
-                  content: "$0.00",
+                  childTag: 'td',
+                  content: '$0.00',
                   childTagAttributes:
                     'data-th="Shipping & Handling (Expedited Shipping - UPS Worldwide Expedited):" role="gridcell"',
                 },
               ],
             },
             {
-              rowTag: "tr",
+              rowTag: 'tr',
               rowTagAttributes: 'role="row"',
               childTags: [
                 {
-                  childTag: "th",
-                  content: "Tax",
+                  childTag: 'th',
+                  content: 'Tax',
                   childTagAttributes:
                     'colspan=\'3\' role="rowheader" scope="row"',
                 },
                 {
-                  childTag: "td",
-                  content: "$0.00",
+                  childTag: 'td',
+                  content: '$0.00',
                   childTagAttributes: 'data-th="Tax:" role="gridcell"',
                 },
               ],
             },
             {
-              rowTag: "tr",
+              rowTag: 'tr',
               rowTagAttributes: 'role="row"',
               childTags: [
                 {
-                  childTag: "th",
-                  content: "Grand Total",
+                  childTag: 'th',
+                  content: 'Grand Total',
                   childTagAttributes:
                     'colspan=\'3\' role="rowheader" scope="row"',
                 },
                 {
-                  childTag: "td",
-                  content: "$480",
+                  childTag: 'td',
+                  content: '$480',
                   childTagAttributes: 'data-th="Grand Total:" role="gridcell"',
                 },
               ],
@@ -279,9 +279,9 @@ module.exports = {
       ],
     },
     submitBtn: {
-      tag: "button",
-      text: "Place order",
-      class: "button--fluid margin-top-m",
+      tag: 'button',
+      text: 'Place order',
+      class: 'button--fluid margin-top-m',
       attributes: 'type="submit"',
     },
   },
