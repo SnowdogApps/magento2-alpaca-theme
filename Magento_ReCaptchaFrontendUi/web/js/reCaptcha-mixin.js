a47f3ab37fe78cd946f8bc5f89767026b26ee9c2
Michał Romaszkin <michal.romaszkin@snow.dog>
2021-10-11 10:42:31 +0200

    #81212: Fix indentation & use visually hidden mixin

--- Magento_ReCaptchaFrontendUi/web/js/reCaptcha-mixin.js
+++ Magento_ReCaptchaFrontendUi/web/js/reCaptcha-mixin.js
@@ -4,82 +4,82 @@ define([
   'Magento_ReCaptchaFrontendUi/js/registry',
 ],
   function ($, _, registry) {
-  'use strict';
+    'use strict';
 
-  var mixin = {
-
-    // SNOWDOG CHANGES
-    // Add check if Recaptcha is disabled
-    getIsInvisibleRecaptcha: function () {
-      if (this.settings) {
-        return this.settings.invisible;
-      }
-    },
-
-    initCaptcha: function () {
-      var $parentForm,
-        $wrapper,
-        $reCaptcha,
-        widgetId,
-        parameters;
-
-      if (this.captchaInitialized) {
-        return;
-      }
+    var mixin = {
 
       // SNOWDOG CHANGES
       // Add check if Recaptcha is disabled
-      if (typeof this.settings === 'undefined') {
-        return;
-      }
+      getIsInvisibleRecaptcha: function () {
+        if (this.settings) {
+          return this.settings.invisible;
+        }
+      },
 
-      this.captchaInitialized = true;
+      initCaptcha: function () {
+        var $parentForm,
+          $wrapper,
+          $reCaptcha,
+          widgetId,
+          parameters;
 
-      /*
-       * Workaround for data-bind issue:
-       * We cannot use data-bind to link a dynamic id to our component
-       * See:
-       * https://stackoverflow.com/questions/46657573/recaptcha-the-bind-parameter-must-be-an-element-or-id
-       *
-       * We create a wrapper element with a wrapping id and we inject the real ID with jQuery.
-       * In this way we have no data-bind attribute at all in our reCAPTCHA div
-       */
-      $wrapper = $('#' + this.getReCaptchaId() + '-wrapper');
-      $reCaptcha = $wrapper.find('.g-recaptcha');
-      $reCaptcha.attr('id', this.getReCaptchaId());
+        if (this.captchaInitialized) {
+          return;
+        }
 
-      $parentForm = $wrapper.parents('form');
+        // SNOWDOG CHANGES
+        // Add check if Recaptcha is disabled
+        if (typeof this.settings === 'undefined') {
+          return;
+        }
 
-      parameters = _.extend(
-        {
-          'callback': function (token) { // jscs:ignore jsDoc
-            this.reCaptchaCallback(token);
-            this.validateReCaptcha(true);
-          }.bind(this),
-          'expired-callback': function () {
-            this.validateReCaptcha(false);
-          }.bind(this)
-        },
-        this.settings.rendering
-      );
+        this.captchaInitialized = true;
 
-      if (parameters.size === 'invisible' && parameters.badge !== 'inline') {
-        nonInlineReCaptchaRenderer.add($reCaptcha, parameters);
-      }
+        /*
+         * Workaround for data-bind issue:
+         * We cannot use data-bind to link a dynamic id to our component
+         * See:
+         * https://stackoverflow.com/questions/46657573/recaptcha-the-bind-parameter-must-be-an-element-or-id
+         *
+         * We create a wrapper element with a wrapping id and we inject the real ID with jQuery.
+         * In this way we have no data-bind attribute at all in our reCAPTCHA div
+         */
+        $wrapper = $('#' + this.getReCaptchaId() + '-wrapper');
+        $reCaptcha = $wrapper.find('.g-recaptcha');
+        $reCaptcha.attr('id', this.getReCaptchaId());
 
-      // eslint-disable-next-line no-undef
-      widgetId = grecaptcha.render(this.getReCaptchaId(), parameters);
-      this.initParentForm($parentForm, widgetId);
+        $parentForm = $wrapper.parents('form');
 
-      registry.ids.push(this.getReCaptchaId());
-      registry.captchaList.push(widgetId);
-      registry.tokenFields.push(this.tokenField);
+        parameters = _.extend(
+          {
+            'callback': function (token) { // jscs:ignore jsDoc
+              this.reCaptchaCallback(token);
+              this.validateReCaptcha(true);
+            }.bind(this),
+            'expired-callback': function () {
+              this.validateReCaptcha(false);
+            }.bind(this)
+          },
+          this.settings.rendering
+        );
 
-    },
-  };
+        if (parameters.size === 'invisible' && parameters.badge !== 'inline') {
+          nonInlineReCaptchaRenderer.add($reCaptcha, parameters);
+        }
 
-  return function (target) {
-    return target.extend(mixin);
-  }
+        // eslint-disable-next-line no-undef
+        widgetId = grecaptcha.render(this.getReCaptchaId(), parameters);
+        this.initParentForm($parentForm, widgetId);
 
-});
+        registry.ids.push(this.getReCaptchaId());
+        registry.captchaList.push(widgetId);
+        registry.tokenFields.push(this.tokenField);
+
+      },
+    };
+
+    return function (target) {
+      return target.extend(mixin);
+    }
+
+  });
