d8ce35db42a12f34fc8e26662dc73e935238ef0c
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2021-10-25 11:27:06 +0200

    82620 - Add selected store info and styles for tabs

--- Snowdog_Components/components/Organisms/store-pickup/store-pickup.config.js
+++ Snowdog_Components/components/Organisms/store-pickup/store-pickup.config.js
@@ -1,18 +1,8 @@
 module.exports = {
   title: 'Header module',
   context: {
-    tabShippingButton: {
-      tag: 'button',
-      class: 'tab__trigger',
-      text: 'Shipping',
-      attributes: 'type="button"'
-    },
-    tabPickInStoreButton: {
-      tag: 'button',
-      class: 'tab__trigger',
-      text: 'Pick in store',
-      attributes: 'type="button"'
-    },
+    tabShippingButtonText: 'Shipping',
+    tabPickInStoreButtonText: 'Pick in store',
     title: {
       tag: 'span',
       class: 'heading heading--second-level',
@@ -23,12 +13,30 @@ module.exports = {
       class: '',
       text: 'Select other',
       attributes: 'type="button"'
+    },
+    selectedStore: {
+      name: {
+        tag: 'p',
+        class: '',
+        text: 'Good Wood Store'
+      },
+      address: {
+        tag: 'p',
+        class: '',
+        text: '119 Timberbrook Lane <br> Denver, Colorado 80216 <br> United States <br> 970-355-8188'
+      },
+      description: {
+        tag: 'p',
+        class: '',
+        text: 'The store is located next to the church.'
+      }
     }
   },
   variants: [
     {
       name: 'no-selection',
       context: {
+        selectedStore: false,
         noSelection: {
           tag: 'p',
           class: '',
