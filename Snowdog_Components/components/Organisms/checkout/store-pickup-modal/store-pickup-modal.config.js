88a9f7fe9938ca25dddb5d28c3c42daed44f8c9c
Damian Fiałkiewicz <damian.fialkiewicz@snow.dog>
2021-10-26 10:52:40 +0200

    82620 - Add table with store select

--- Snowdog_Components/components/Organisms/checkout/store-pickup-modal/store-pickup-modal.config.js
+++ Snowdog_Components/components/Organisms/checkout/store-pickup-modal/store-pickup-modal.config.js
@@ -3,7 +3,7 @@ module.exports = {
     search: {
       input: {
         attributes: '',
-        class: '',
+        class: 'store-pickup-modal__search-field',
         label: {
           attributes: '',
           text: 'Search',
@@ -20,15 +20,79 @@ module.exports = {
       },
       button: {
         tag: 'button',
-        class: '',
+        class: 'store-pickup-modal__search-button',
         attributes: 'type="submit"',
         text: 'Search'
       }
     },
     description: {
       tag: 'p',
-      class: '',
+      class: 'store-pickup-modal__paragraph',
       text: 'Please provide postcode or city name to find nearest pickup locations.'
+    },
+    storesTable: {
+      captionText: 'This is a table. Binded text in caption should explain what kind of data this table presents. Can be also a longer text with inline HTML elements inside',
+      class: 'store-pickup-modal__table margin-0',
+      mainTags: [
+        {
+          mainTag: 'thead',
+          rowTags: [
+            {
+              rowTag: 'tr',
+              rowTagAttributes: 'role="row"',
+              childTags: [
+                {
+                  childTag: 'th',
+                  childTagAttributes: 'scope="col" role="columnheader"',
+                  content: 'Location Details'
+                },
+                {
+                  childTag: 'th',
+                  childTagAttributes: 'scope="col" role="columnheader"',
+                  content: 'Action'
+                }
+              ]
+            },
+          ]
+        },
+        {
+          mainTag: 'tbody',
+          rowTags: [
+            {
+              rowTag: 'tr',
+              rowTagAttributes: 'role="row"',
+              childTags: [
+                {
+                  childTag: 'td',
+                  childTagAttributes: 'data-th="Location Details" role="gridcell"',
+                  content: 'Good Wood Store<br>119 Timberbrook Lane<br>Denver, Colorado 80216<br>United States<br><a href="tel:970-355-8188">970-355-8188</a>'
+                },
+                {
+                  childTag: 'td',
+                  childTagAttributes: 'data-th="Action" role="gridcell"',
+                  content: '<button type="button" class="button">Ship Here</button>'
+                }
+              ]
+            },
+            {
+              rowTag: 'tr',
+              rowTagAttributes: 'role="row"',
+              childTags: [
+                {
+                  childTag: 'td',
+                  childTagAttributes: 'data-th="Location Details" role="gridcell"',
+                  content: 'Good Wood Store 2<br>120 Timberbrook Lane<br>Denver, Colorado 80216<br>United States<br><a href="tel:970-355-8188">970-355-8188</a>'
+                },
+                {
+                  childTag: 'td',
+                  childTagAttributes: 'data-th="Action" role="gridcell"',
+                  content: '<button type="button" class="button">Ship Here</button>'
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
   }
 };
