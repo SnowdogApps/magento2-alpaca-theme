221326d299010e41183a8490b1923483d625bf56
Ola <ola.frankowska@snow.dog>
2022-03-01 14:11:29 +0100

    #87146 fix PDP - not working Yotpo button "Write a review" under the product's name

--- Yotpo_Yotpo/web/js/reviews-navigation.babel.js
+++ Yotpo_Yotpo/web/js/reviews-navigation.babel.js
@@ -3,11 +3,30 @@ define([], function () {
 
   class ReviewsNavigation {
     constructor(element) {
-      this.summaryLinks = element.querySelectorAll('.yotpo-bottomline');
+      this.element = element;
+      this.summaryLinks = [];
+      this.summaryLinksClass = '.yotpo-bottomline';
       this.tab = document.querySelector('.tab__title--reviews');
 
       if (this.tab) {
-        this.setListeners();
+        this.init();
+      }
+    }
+
+    init() {
+      const self = this;
+      const getSummaryLinks = setInterval(() => {
+        if (self.summaryLinks.length > 0) {
+          clearInterval(getSummaryLinks);
+          self.setListeners();
+        }
+        else {
+          self.summaryLinks = self.element.querySelectorAll(self.summaryLinksClass);
+        }
+      }, 500);
+
+      if (this.tab) {
+        getSummaryLinks;
       }
     }
 
