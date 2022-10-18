45ea71e577513c5dc5fbd953b3fc3d32681e0f4c
Paweł Kajdasz <pawel.kajdasz@snow.dog>
2022-10-19 00:27:22 +0200

    #92750 - loader mixin comments

--- Magento_Theme/web/js/mixins/loader-mixin.js
+++ Magento_Theme/web/js/mixins/loader-mixin.js
@@ -4,11 +4,16 @@ define([
   'use strict';
 
   return function (widget) {
+    /*** replace magento loader widget template
+      magento triggers:
+        show loader: $('body').trigger('processStart');
+        hide loader: $('body').trigger('processStop');
+    ***/
     $.widget('mage.loader', widget.loader, {
       options: {
         template:
           '<div class="loading-mask" data-role="loader">' +
-            '<div class="loader loader--visible dupa">' +
+            '<div class="loader loader--visible">' +
               '<div class="loader__icon"></div>' +
             '</div>' +
           '</div>'
@@ -16,6 +21,7 @@ define([
     });
 
     return {
+      /* must return all widgets */
       loader: $.mage.loader,
       loaderAjax: $.mage.loaderAjax
     };
